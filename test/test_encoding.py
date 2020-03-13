69785001fc439d13a6f4c9cc1e659fad83e00d11
IsmaelCesar <leamscesar@gmail.com>
2020-03-13 09:17:55 -0300

    Test procedures
    Adding other test cases to the test module.
    
    Changes to be submitted:
    	modified:   test_encoding.py

--- test/test_encoding.py
+++ test/test_encoding.py
@@ -59,6 +59,40 @@ class TestCircuitCreation(TestCase):
         for exp_amplitude, out_amplitude in zip(input_vector, out_state):
             self.assertTrue(np.abs(exp_amplitude - out_amplitude) < 10 ** (-5))
 
+    def test_mottonen_initialization_random_4qubtis(self):
+        """
+        exemplo de teste
+        :return:
+        """
+        input_vector = np.random.rand(16)
+        input_vector = input_vector / np.linalg.norm(input_vector)
+        quantum_circuit = mottonen_quantum_circuit(input_vector)
+        backend_sim = Aer.backends('statevector_simulator')[0]
+        job = execute(quantum_circuit, backend_sim)
+        result = job.result()
+
+        out_state = result.get_statevector(quantum_circuit)
+
+        for exp_amplitude, out_amplitude in zip(input_vector, out_state):
+            self.assertTrue(np.abs(exp_amplitude - out_amplitude) < 10 ** (-5))
+
+    def test_mottonen_random_sized_feature_vector(self):
+        """
+        Testing mottonen's quantum circuit resulting state vector for a randomly sized feature
+        vector
+        :return:
+        """
+
+        input_vector = np.random.uniform(low=-10, high=10, size=randint(2, 17))
+        input_vector = input_vector / np.linalg.norm(input_vector)
+        input_vector_resized = _resize_feature_vectors(input_vector)
+        quantum_circuit = mottonen_quantum_circuit(input_vector)
+        backend_sim = Aer.backends('statevector_simulator')[0]
+        job = execute(quantum_circuit, backend_sim)
+        result = job.result()
+        out_state = result.get_statevector()
+        for exp_amplitude, out_amplitude in zip(input_vector_resized, out_state):
+            self.assertTrue(np.abs(exp_amplitude - out_amplitude) < 10 ** (-5))
     
 if __name__ == "__main__":
     unittest.main()
