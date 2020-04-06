c74fb1a620dbd26b267e909f8480ea531a13cd44
IsmaelCesar <leamscesar@gmail.com>
2020-04-06 10:27:00 -0300

    Storing a dataset with complex_valued feature vectors
    Adding the test of storing a dataset with
    
    Changes to be submitted:
    	modified:   test/test_encoding.py

--- test/test_encoding.py
+++ test/test_encoding.py
@@ -10,6 +10,7 @@ from qiskit import execute, Aer
 import qclib.encoding as encoding
 from qclib.utils import transform_dataset
 
+
 class TestMottonenCircuit(TestCase):
     """
     Class dedicated to the implementation of the unittests for the creation of the quantum circuit.
@@ -101,7 +102,7 @@ class TestParksCircuit(TestCase):
     quantum circuit that implements the Park's method
     """
 
-    def test_parks_method_with_a_dataset_one_feature_vector(self):
+    def test_dataset_with_one_feature_vector(self):
         """
             Test the output of the quantum circuit with a dataset with
             only one feature vector.
@@ -126,7 +127,7 @@ class TestParksCircuit(TestCase):
         for returned, target in zip(returned_state_vector, expected_state_vector):
             self.assertTrue((returned - target) < 10**-5)
 
-    def test_parks_method_with_a_dataset_negative_valued_feature_vector(self):
+    def test_dataset_with_negative_valued_feature_vector(self):
         """
             Test the output of the quantum circuit with a dataset with
             only one feature vector with negative values
@@ -155,7 +156,7 @@ class TestParksCircuit(TestCase):
         for returned, target in zip(returned_state_vector, expected_state_vector):
             self.assertTrue((returned - target) < 10**-5)
 
-    def test_parks_method_with_a_dataset_two_feature_vectors(self):
+    def test_dataset_with_two_feature_vectors(self):
         """
             Test the output of the quantum circuit with a dataset with
             two feature vectors.
@@ -178,7 +179,7 @@ class TestParksCircuit(TestCase):
         for returned, target in zip(returned_state_vector, expected_state_vector):
             self.assertTrue((returned - target) < 10 ** -5)
 
-    def test_parks_method_with_a_dataset_two_negative_valued_feature_vectors(self):
+    def test_dataset_with_two_negative_valued_feature_vectors(self):
         """
             Test the output of the quantum circuit with a dataset with
             two negative valued feature vectors.
@@ -201,6 +202,35 @@ class TestParksCircuit(TestCase):
                                            np.sqrt(0.02), np.sqrt(0.03),
                                            np.sqrt(0.1), np.sqrt(0.4),
                                            np.sqrt(0.3), np.sqrt(0.1)])
+
+        for returned, target in zip(returned_state_vector, expected_state_vector):
+            self.assertTrue((returned - target) < 10 ** -5)
+
+    def test_dataset_with_one_complex_valued_feature_vector(self):
+        """
+            Test the output of the quantum circuit
+            for a dataset with one complex valued feature vector
+        :return: None
+        """
+        input_vector = np.array([complex(np.sqrt(0.02), np.sqrt(0.01)),
+                                 complex(np.sqrt(0.4), np.sqrt(0.07)),
+                                 complex(np.sqrt(0.1), np.sqrt(0.08)),
+                                 complex(np.sqrt(0.3), np.sqrt(0.02))])
+
+        transformed_data, n_data_qbits = transform_dataset(input_vector)
+
+        circuit = encoding.park_quantum_circuit(transformed_data, n_data_qbits)
+
+        returned_state_vector = encoding.parks_state_vector_post_selection(circuit)
+
+        expected_state_vector = [0, 0,
+                                 0, 0,
+                                 0, 0,
+                                 0, 0,
+                                 complex(np.sqrt(0.02), np.sqrt(0.01)), complex(np.sqrt(0.4), np.sqrt(0.07)),
+                                 complex(np.sqrt(0.1), np.sqrt(0.08)), complex(np.sqrt(0.3), np.sqrt(0.02)),
+                                 0, 0,
+                                 0, 0]
 
         for returned, target in zip(returned_state_vector, expected_state_vector):
             self.assertTrue((returned - target) < 10 ** -5)
