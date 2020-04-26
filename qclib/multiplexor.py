5acfbf5c95767e310bb2608f74df43f9361a4328
Adenilton Silva <ajsilva@cin.ufpe.br>
2020-04-26 20:54:29 -0300

    fix multiplexor

--- qclib/multiplexor.py
+++ qclib/multiplexor.py
@@ -1,5 +1,8 @@
-import numpy as np
+'''
+Quantum Multiplexor
+'''
 
+import numpy as np
 from qiskit import QuantumRegister, QuantumCircuit
 from qiskit.circuit import Instruction
 from qiskit.extensions.standard.ry import RYGate
@@ -8,7 +11,7 @@ from qiskit.extensions.standard.rz import RZGate
 
 class Multiplexor(Instruction):
     """
-    Rotation R_k Multiplexor arXiv:quant-ph/0406176
+    Rotation R_k Multiplexor
     """
     def __init__(self, params, gate):
         """
@@ -18,12 +21,19 @@ class Multiplexor(Instruction):
         self._gate = gate
         self.params = params
         self.num_qubits = int(np.log2(len(params)) + 1)
+        self._circuit = None
+        self.cnot_control = None
+        self.multiplexor_angles = None
 
         super().__init__("multiplexor", self.num_qubits, 0, params)
 
     def _define(self):
+
         self._compute_cnot_control()
-        self.initialize()
+        self._compute_multiplexor_angles()
+        self._mirror_angles()
+        self._initialize()
+
         self.definition = self._circuit.data
 
     def _compute_cnot_control(self):
@@ -41,42 +51,57 @@ class Multiplexor(Instruction):
             pos = pos // 2
 
     def _compute_multiplexor_angles(self):
+        self.multiplexor_angles = self.params.copy()
+        left = 0
+        right = len(self.params)
+        mid = (left + right) // 2
+        self._compute_multiplexor_angles2(left, mid, right)
 
-        local_angles = self.params
-        multiplexor_angles = len(local_angles) * [0]
+    def _compute_multiplexor_angles2(self, left, mid, right):
+        for k in range(0, mid-left):
+            v_left = (self.multiplexor_angles[k+left] + self.multiplexor_angles[k+mid]) / 2
+            v_right = (self.multiplexor_angles[k+left] - self.multiplexor_angles[k+mid]) / 2
+            self.multiplexor_angles[k+left] = v_left
+            self.multiplexor_angles[k+mid] = v_right
 
-        weights1 = np.array([(-1) ** k for k in range(len(local_angles))])
-        weights2 = np.array([(-1) ** ((2 & (k + 1)) // 2) for k in range(len(local_angles))])
+        if mid - left > 1:
+            mid_right = (mid + right) // 2
+            mid_left = (left + mid) // 2
 
-        for k, _ in enumerate(local_angles):
-            if k != 0 and k % 2 == 0:
-                local_angles = np.multiply(local_angles, weights2)
-            if k % 2 == 1:
-                local_angles = np.multiply(local_angles, weights1)
-            multiplexor_angles[k] = sum(local_angles)/len(local_angles)
+            self._compute_multiplexor_angles2(left, mid_left, mid)
+            self._compute_multiplexor_angles2(mid, mid_right, right)
 
-        end_multiplexor_angles = multiplexor_angles[:len(self.params) // 2]
-        multiplexor_angles.reverse()
-        end_multiplexor_angles = end_multiplexor_angles + multiplexor_angles[:len(self.params) // 2]
-        return end_multiplexor_angles
+    def _mirror_angles(self):
+        n_angles = len(self.multiplexor_angles)
+        if n_angles >= 4:
+            j = 2
+            while j <= n_angles // 2:
+                for k in range(j, n_angles, 2*j):
+                    self.multiplexor_angles[k:k+j] = reversed(self.multiplexor_angles[k:k+j])
+                j = j * 2
 
-    def initialize(self):
-        qr = QuantumRegister(self.num_qubits)
-        self._circuit = QuantumCircuit(qr)
+    def _initialize(self):
+        quantum_register = QuantumRegister(self.num_qubits)
+        self._circuit = QuantumCircuit(quantum_register)
 
-        m_angles = self._compute_multiplexor_angles()
-
+        m_angles = self.multiplexor_angles
         for k, _ in enumerate(self.params):
-            self._circuit.append(self._gate(m_angles[k]), [qr[0]])
+            self._circuit.append(self._gate(m_angles[k]), [quantum_register[0]])
             if self.cnot_control[k] != 0:
-                self._circuit.cx(qr[self.cnot_control[k]], qr[0])
+                self._circuit.cx(quantum_register[self.cnot_control[k]], quantum_register[0])
 
-        self._circuit.cx(qr[self.num_qubits - 1], qr[0])
+        self._circuit.cx(quantum_register[self.num_qubits - 1], quantum_register[0])
 
     def ry_multiplexor(self, params, qubits):
+        """
+        Apply Ry multiplexor with angles params arXiv:quant-ph/0406176
+        """
         return self.append(Multiplexor(params, RYGate), qubits)
 
     def rz_multiplexor(self, params, qubits):
+        """
+        Apply Ry multiplexor with angles params arXiv:quant-ph/0406176
+        """
         return self.append(Multiplexor(params, RZGate), qubits)
 
     QuantumCircuit.ry_multiplexor = ry_multiplexor
