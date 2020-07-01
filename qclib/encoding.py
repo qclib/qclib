4f825e5e44d61704bf610e61592e81755f29e8ef
IsmaelCesar <leamscesar@gmail.com>
2020-07-01 10:05:01 -0300

    Including phase encoding to Möttönen procedure
    - Applying global phase to the state before performing the
    reverse procedure of phase equalization
    - Intercalating Ry with Rz
    
    on branch mottonen_phase
    Changes to be submitted:
    	modified:   qclib/encoding.py

--- qclib/encoding.py
+++ qclib/encoding.py
@@ -30,6 +30,8 @@ class InitializerUniformlyRotation(Initializer):
             params = params / features_norm
 
         self._angles_tree = []
+        self._phases_tree = []
+        self._phase_vector = []
         self.params = params
         self.num_qubits = int(np.log2(len(params)))
         self._circuit = QuantumCircuit(self.num_qubits)
@@ -41,9 +43,7 @@ class InitializerUniformlyRotation(Initializer):
             Extract the phase of any complex entry in the feature vector
         :param input_vector: The feature vector to be encoded in the quantum state,
                              it is expected to be normalized.
-        :return: two numpy arrays
-                 First with the norm of the complex entry
-                 Second with its phase
+        :return: The phase of the entries in the complex
 
         """
         phases = []
@@ -54,28 +54,7 @@ class InitializerUniformlyRotation(Initializer):
                 input_vector[idx_entry] = np.linalg.norm(entry)
             else:
                 phases += 0
-        return input_vector, np.array(phases)
-
-    def _get_angle_computation(self, phase_vector, start, end, skip):
-        """
-        Performs two-by-two subtraction between the phase angles in the phase vector
-        using skip to define which angles keep a positive signal and which keep a negative
-
-        Let "phase_idx" be the phase index in the phase_vector. And let "a" be the angle
-
-            a_(phase_idx + skip) - a_(phase_idx)
-
-        :param phase_vector: list with the extracted phases from the complex features
-        :param start: index of the starting angle of the two-by-two subtraction
-        :param end: index of the ending angle of the two-by-two subtraction
-        :param skip: the skiping index to define which term will be the positive term
-                    the subtracion
-        :return: sumation of a two-by-two subtraction of angles in the phase_vector
-        """
-        sumation = 0
-        for phase_idx in range(start, end):
-            sumation += (phase_vector[phase_idx + skip] - phase_vector[phase_idx])
-        return sumation
+        return phases
 
     def _apply_global_phase(self, phase_vector):
         """
@@ -101,25 +80,49 @@ class InitializerUniformlyRotation(Initializer):
         self._circuit.x(ms_qubit)
         self._circuit.u1(omega, ms_qubit)
 
-    def _compute_phase_equalization_angles(self, phase_vector, n_qubits):
+    def _get_angle_computation(self, phase_vector, start, end, skip):
         """
+        Performs two-by-two subtraction between the phase angles in the phase vector
+        using skip to define which angles keep a positive signal and which keep a negative
+
+        Let "phase_idx" be the phase index in the phase_vector. And let "a" be the angle
+
+            a_(phase_idx + skip) - a_(phase_idx)
 
         :param phase_vector: list with the extracted phases from the complex features
-        :param n_qubits: number of qubits necessary to encode the phases
-        :return: Computed angles for the phase equalization algorithm
+        :param start: index of the starting angle of the two-by-two subtraction
+        :param end: index of the ending angle of the two-by-two subtraction
+        :param skip: the skiping index to define which term will be the positive term
+                    the subtracion
+        :return: sumation of a two-by-two subtraction of angles in the phase_vector
+        """
+        sumation = 0
+        for phase_idx in range(start, end):
+            sumation += (phase_vector[phase_idx + skip] - phase_vector[phase_idx])
+        return sumation
+
+    def _compute_phase_equalization_angles(self, phase_vector):
+        """
+        Extracts the phase from complex entries in the input vector
+        Computes the phase equalization angles to be used when applying the cascading
+        controlled Rz
+
+        :param input_vector: The feature vector to be encoded in the quantum state,
+                             it is expected to be normalized.
+        :return: None
         """
         tree_level = 0
         phase_angles = []
-        while tree_level < n_qubits:
+        while tree_level < self.num_qubits:
 
-            main_factor = 1 / 2 ** (n_qubits - tree_level)
+            main_factor = 1 / 2 ** (self.num_qubits - tree_level)
             # Defining how many indices must be skiped before applying subtraction
-            skip = 2 ** (n_qubits - tree_level - 1)
+            skip = 2 ** (self.num_qubits - tree_level - 1)
             # Defining start and  end indices
             start = 0
-            end = 2 ** (n_qubits - tree_level - 1)
+            end = 2 ** (self.num_qubits - tree_level - 1)
             # defining how many cells the start and end indices must be shifted
-            offset = 2 ** (n_qubits - tree_level)
+            offset = 2 ** (self.num_qubits - tree_level)
             n_nodes = 2 ** tree_level
             for node in range(n_nodes):
                 phase_angles.append(main_factor * self._get_angle_computation(phase_vector, start, end, skip))
@@ -127,7 +130,7 @@ class InitializerUniformlyRotation(Initializer):
                 end += offset
 
             tree_level += 1
-        return phase_angles
+        self._phases_tree = phase_angles
 
     def _recursive_compute_amplitude_angles(self, input_vector):
         """
@@ -153,7 +156,7 @@ class InitializerUniformlyRotation(Initializer):
             for value in angles:
                 self._angles_tree.append(value)
 
-    def _apply_controlled_rotations(self, controls, angle, n_qubits):
+    def _apply_controlled_rotations(self, controls, angle, phase, n_qubits):
         """
         This procedure applies controlled rotations using a tuple
         with the states of the qubits (eg.: controls = (x, y, z),
@@ -161,10 +164,14 @@ class InitializerUniformlyRotation(Initializer):
         A not gate is applied to a control qubit everytime it's in the state |0> ,
         given the controlled rotations are activated when the control qubit is set to |1>.
 
+        :param phase:
         :param controls: A tuple with one possibility of states of qubits, example: (0,1,0)
         :param angle: Angle to be used in the multi controlled rotation
         :param n_qubits: (int) Number of qubits in the quantum circtuin
         """
+
+        # Define global phase
+        self._apply_global_phase()
 
         n_controls = len(controls)
         control_qubit_indexes = list(range(n_controls))
@@ -184,6 +191,11 @@ class InitializerUniformlyRotation(Initializer):
         target = reg[n_qubits - n_controls - 1]
         self._circuit.mcry(angle, control_qubit_objects_list, target, None, mode='noancilla')
 
+        # If phase == 0 then the Rz becomes Identity,
+        # thus increasing the depth of the circuit unnecessarily
+        if phase != 0:
+            self._circuit.mcrz(phase, control_qubit_objects_list, target)
+
         for i, ctrl in enumerate(controls):
             if ctrl == 0:
                 self._circuit.x(n_qubits - i - 1)
@@ -194,13 +206,18 @@ class InitializerUniformlyRotation(Initializer):
         Building a coherent superposition from the ground state with the features encoded in the phase.
         """
         angles = self._angles_tree
+        phases = self._phases_tree
 
         n_qubits = int(np.ceil(np.log2(len(angles) + 1)))
 
+        self._apply_global_phase(self._phase_vector)
+
         # Building Circuit
         current_value = angles.pop(0)
+        current_phase = phases.pop(0)
 
         self._circuit.ry(current_value, n_qubits - 1)
+        self._circuit.rz(current_phase, n_qubits - 1)
 
         for i in range(1, n_qubits):
 
@@ -209,7 +226,8 @@ class InitializerUniformlyRotation(Initializer):
 
             for controls in c_qubits:
                 current_value = angles.pop(0)
-                self._apply_controlled_rotations(controls, current_value, n_qubits)
+                current_phase = phases.pop(0)
+                self._apply_controlled_rotations(controls, current_value, current_phase, n_qubits)
 
     def _define(self):
         """
@@ -221,6 +239,8 @@ class InitializerUniformlyRotation(Initializer):
         """
 
         self._recursive_compute_amplitude_angles(self.params)
+        self._phase_vector = self._extract_phase_from_complex(self.params)
+        self._compute_phase_equalization_angles(self._phase_vector)
         self._create_circuit()
         self.definition = self._circuit.data
 
