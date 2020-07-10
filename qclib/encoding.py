fa7bc4f88714cd33b9e030adafb58d0e12e3f1bc
IsmaelCesar <leamscesar@gmail.com>
2020-07-10 10:16:28 -0300

    Complex number encoding arXiv:quant-ph/0407010 without gray code
    * Data structures for phase equalization
    
    - Defining procedures for extracting the phase
    from complex entries
    - Creating procedure that derives a heap data structure
    for phase equalization angles
    
    on branch mottonen_phase
    
    Changes to be submitted:
    	modified:   qclib/encoding.py
    
    * Applying global phase to quantum state
    
    - Creating a basic procedure combining x and u1 operators
    to apply a global phase operation in order to the phase
    equalization procedure to work properly
    
    on branch mottonen_phase
    Changes to be submitted:
    	modified:   qclib/encoding.py
    
    * Including phase encoding to Möttönen procedure
    
    - Applying global phase to the state before performing the
    reverse procedure of phase equalization
    - Intercalating Ry with Rz
    
    on branch mottonen_phase
    Changes to be submitted:
    	modified:   qclib/encoding.py
    
    * Fixing bugs detected by current unit tests
    
    * Test cases and Phase encoding
    
    - Fixing bugs detected by unittest
    
    on branch mottonen_phase
    Changes to be submitted:
    	modified:   qclib/encoding.py
    	modified:   test/test_encoding.py
    
    * Minor alterations
    
    * Adding more test cases
    
    Adding more test cases for testing:
    - The phases extraction from complex numbers
    - The calculation of the phases tree
    - The application of the global phase operator to the quantum state
    
    on branch mottonen_phase
    Changes to be submitted:
    	modified:   test/test_encoding.py
    
    * Changing way the global phase is encoded
    
    Changing how the angles in the global phases are encoded
    based on which phases should be in which state based on
    the operation that must be performed to the root of the angle tree.
    
    On branch mottonen_phase
    Changes to be submitted:
    	modified:   qclib/encoding.py
    	modified:   test/test_encoding.py
    
    * First phase encoding test successful
    
    Implementend an alternative phase gate by combining a set of
    u1 and x operators. This alternative fix makes the circuit
    deeper, but at least the first test with phase encoding was
    successful.
    
    On branch mottonen_phase
    Changes to be submitted:
    	modified:   qclib/encoding.py
    
    * Adding new test cases to the test database
    
    Adding new test cases changing the signs and the
    size of the input vector.
    All test cases added were successful.
    
    On branch mottonen_phase
    changes to bbe submitted:
    	modified:   test/test_encoding.py
    
    * Applying changes suggested on pull request
    
    on branch mottonen_phase
    changes to be submitted:
    	modified:   test/test_encoding.py

--- qclib/encoding.py
+++ qclib/encoding.py
@@ -15,7 +15,6 @@ class Initializer(ABC, Instruction):
         pass
 
 
-
 class InitializerUniformlyRotation(Initializer):
     """
     State preparation arXiv:quant-ph/0407010
@@ -31,13 +30,118 @@ class InitializerUniformlyRotation(Initializer):
             params = params / features_norm
 
         self._angles_tree = []
+        self._phases_tree = []
+        self._phases_vector = []
         self.params = params
         self.num_qubits = int(np.log2(len(params)))
         self._circuit = QuantumCircuit(self.num_qubits)
 
         super().__init__("initialize UR", self.num_qubits, 0, self.params)
 
-    def _recursive_compute_angles(self, input_vector):
+    def _extract_phase_from_complex(self, input_vector):
+        """
+            Extract the phase of any complex entry in the feature vector
+        :param input_vector: The feature vector to be encoded in the quantum state,
+                             it is expected to be normalized.
+        :return:The input_vector with the amplitudes of the states
+                The phase of the complex entries
+
+        """
+        phases = []
+        amplitudes = []
+
+        for idx_entry, entry in enumerate(input_vector):
+            if isinstance(entry, complex):
+                phases += [np.log(entry).imag]
+                amplitudes += [np.abs(entry)]
+            else:
+                phases += [0]
+                amplitudes += [input_vector[idx_entry]]
+        return amplitudes, np.array(phases)
+
+    def _apply_global_phase(self, phase_vector, angle):
+        """
+            Apply global phase to the most significant qubit.
+            According to arXiv:quant-ph/0407010v1 after the phase equalization
+            the global phase of the state is the mean of the all phase angles.
+            That is
+
+            exp(i * 1 / 2**n_qubits * sum(phase_vector))
+
+            In order to compute the reversed process of phase equalization
+            the quantum state must have the presented global phase
+
+
+        :param phase_vector: list with the extracted phases from the complex features
+        :return: None
+        """
+        omega = 1 / 2**self.num_qubits * np.sum(phase_vector)
+
+        phi_plus = omega + angle
+        phi_minus = omega - angle
+
+        ms_qubit = self.num_qubits - 1
+        # Phase |1>|0...0>
+        self._circuit.u1(phi_plus, ms_qubit)
+
+        # Phase |0>|0...0>
+        self._circuit.x(ms_qubit)
+        self._circuit.u1(phi_minus, ms_qubit)
+        self._circuit.x(ms_qubit)
+
+    def _get_angle_computation(self, phase_vector, start, end, skip):
+        """
+        Performs two-by-two subtraction between the phase angles in the phase vector
+        using skip to define which angles keep a positive signal and which keep a negative
+
+        Let "phase_idx" be the phase index in the phase_vector. And let "a" be the angle
+
+            a_(phase_idx + skip) - a_(phase_idx)
+
+        :param phase_vector: list with the extracted phases from the complex features
+        :param start: index of the starting angle of the two-by-two subtraction
+        :param end: index of the ending angle of the two-by-two subtraction
+        :param skip: the skiping index to define which term will be the positive term
+                    the subtracion
+        :return: sumation of a two-by-two subtraction of angles in the phase_vector
+        """
+        sumation = 0
+        for phase_idx in range(start, end):
+            sumation += (phase_vector[phase_idx + skip] - phase_vector[phase_idx])
+        return sumation
+
+    def _compute_phase_equalization_angles(self, phase_vector):
+        """
+        Extracts the phase from complex entries in the input vector
+        Computes the phase equalization angles to be used when applying the cascading
+        controlled Rz
+
+        :param input_vector: The feature vector to be encoded in the quantum state,
+                             it is expected to be normalized.
+        :return: None
+        """
+        tree_level = 0
+        phase_angles = []
+        while tree_level < self.num_qubits:
+
+            main_factor = 1 / 2 ** (self.num_qubits - tree_level)
+            # Defining how many indices must be skiped before applying subtraction
+            skip = 2 ** (self.num_qubits - tree_level - 1)
+            # Defining start and  end indices
+            start = 0
+            end = 2 ** (self.num_qubits - tree_level - 1)
+            # defining how many cells the start and end indices must be shifted
+            offset = 2 ** (self.num_qubits - tree_level)
+            n_nodes = 2 ** tree_level
+            for node in range(n_nodes):
+                phase_angles.append(main_factor * self._get_angle_computation(phase_vector, start, end, skip))
+                start += offset
+                end += offset
+
+            tree_level += 1
+        self._phases_tree = phase_angles
+
+    def _recursive_compute_amplitude_angles(self, input_vector):
         """
         :param input_vector: The feature vector to be encoded in the quantum state,
                              it is expected to be normalized.
@@ -57,11 +161,11 @@ class InitializerUniformlyRotation(Initializer):
                         angles.append(2 * np.pi - 2 * np.arcsin(input_vector[k + 1] / norm))
                     else:
                         angles.append(2 * np.arcsin(input_vector[k + 1] / norm))
-            self._recursive_compute_angles(new_input)
+            self._recursive_compute_amplitude_angles(new_input)
             for value in angles:
                 self._angles_tree.append(value)
 
-    def _apply_controlled_rotations(self, controls, angle, n_qubits):
+    def _apply_controlled_rotations(self, controls, angle, phase, n_qubits):
         """
         This procedure applies controlled rotations using a tuple
         with the states of the qubits (eg.: controls = (x, y, z),
@@ -69,6 +173,7 @@ class InitializerUniformlyRotation(Initializer):
         A not gate is applied to a control qubit everytime it's in the state |0> ,
         given the controlled rotations are activated when the control qubit is set to |1>.
 
+        :param phase:
         :param controls: A tuple with one possibility of states of qubits, example: (0,1,0)
         :param angle: Angle to be used in the multi controlled rotation
         :param n_qubits: (int) Number of qubits in the quantum circtuin
@@ -92,6 +197,18 @@ class InitializerUniformlyRotation(Initializer):
         target = reg[n_qubits - n_controls - 1]
         self._circuit.mcry(angle, control_qubit_objects_list, target, None, mode='noancilla')
 
+        # If phase == 0 then the Rz becomes Identity,
+        # thus increasing the depth of the circuit unnecessarily
+        if phase != 0:
+            # Phase |0>
+            self._circuit.x(target)
+            self._circuit.mcu1(-phase, control_qubit_objects_list, target)
+            self._circuit.x(target)
+
+            # Phase |1>
+            self._circuit.mcu1(phase, control_qubit_objects_list, target)
+            # self._circuit.mcrz(phase, control_qubit_objects_list, target)
+
         for i, ctrl in enumerate(controls):
             if ctrl == 0:
                 self._circuit.x(n_qubits - i - 1)
@@ -102,13 +219,17 @@ class InitializerUniformlyRotation(Initializer):
         Building a coherent superposition from the ground state with the features encoded in the phase.
         """
         angles = self._angles_tree
+        phases = self._phases_tree
 
         n_qubits = int(np.ceil(np.log2(len(angles) + 1)))
 
         # Building Circuit
         current_value = angles.pop(0)
+        current_phase = phases.pop(0)
 
         self._circuit.ry(current_value, n_qubits - 1)
+        self._apply_global_phase(self._phases_vector, current_phase)
+        # self._circuit.rz(current_phase, n_qubits - 1)
 
         for i in range(1, n_qubits):
 
@@ -117,7 +238,8 @@ class InitializerUniformlyRotation(Initializer):
 
             for controls in c_qubits:
                 current_value = angles.pop(0)
-                self._apply_controlled_rotations(controls, current_value, n_qubits)
+                current_phase = phases.pop(0)
+                self._apply_controlled_rotations(controls, current_value, current_phase, n_qubits)
 
     def _define(self):
         """
@@ -127,8 +249,9 @@ class InitializerUniformlyRotation(Initializer):
                          it is expected to be normalized.
         :return: Quantum Circuit object generated to perform Mottonen's method
         """
-
-        self._recursive_compute_angles(self.params)
+        amplitues, self._phases_vector = self._extract_phase_from_complex(self.params)
+        self._recursive_compute_amplitude_angles(amplitues)
+        self._compute_phase_equalization_angles(self._phases_vector)
         self._create_circuit()
         self.definition = self._circuit.data
 
--- test/test_encoding.py
+++ test/test_encoding.py
@@ -7,6 +7,7 @@ from random import randint
 import numpy as np
 from qiskit import execute, Aer, QuantumRegister
 from qclib import QuantumCircuit
+from qclib.encoding import InitializerUniformlyRotation
 
 
 class TestCircuitCreation(TestCase):
@@ -37,8 +38,9 @@ class TestCircuitCreation(TestCase):
 
         out_state = result.get_statevector(quantum_circuit)
 
-        for exp_amplitude, out_amplitude in zip(input_vector, out_state):
-            self.assertTrue(np.abs(exp_amplitude - out_amplitude) < 10 ** (-5))
+        # for exp_amplitude, out_amplitude in zip(input_vector, out_state):
+        #    self.assertTrue(np.abs(exp_amplitude - out_amplitude) < 10 ** (-5))
+        self.assertTrue(np.isclose(input_vector, out_state).all())
 
     def test_ur_initialization_basis_state(self):
         """
@@ -63,8 +65,9 @@ class TestCircuitCreation(TestCase):
 
         out_state = result.get_statevector(quantum_circuit)
 
-        for exp_amplitude, out_amplitude in zip(input_vector, out_state):
-            self.assertTrue(np.abs(exp_amplitude - out_amplitude) < 10 ** (-5))
+        # for exp_amplitude, out_amplitude in zip(input_vector, out_state):
+        #    self.assertTrue(np.abs(exp_amplitude - out_amplitude) < 10 ** (-5))
+        self.assertTrue(np.isclose(input_vector, out_state).all())
 
     def test_ur_initialization_random_4qubtis(self):
         """
@@ -82,8 +85,9 @@ class TestCircuitCreation(TestCase):
         result = job.result()
 
         out_state = result.get_statevector(quantum_circuit)
-        for exp_amplitude, out_amplitude in zip(input_vector, out_state):
-            self.assertTrue(np.abs(exp_amplitude - out_amplitude) < 10 ** (-5))
+        # for exp_amplitude, out_amplitude in zip(input_vector, out_state):
+        #    self.assertTrue(np.abs(exp_amplitude - out_amplitude) < 10 ** (-5))
+        self.assertTrue(np.isclose(input_vector, out_state).all())
 
     def test_ur_random_sized_feature_vector(self):
         """
@@ -104,8 +108,9 @@ class TestCircuitCreation(TestCase):
         job = execute(quantum_circuit, backend_sim)
         result = job.result()
         out_state = result.get_statevector()
-        for exp_amplitude, out_amplitude in zip(input_vector, out_state):
-            self.assertTrue(np.abs(exp_amplitude - out_amplitude) < 10 ** (-5))
+        # for exp_amplitude, out_amplitude in zip(input_vector, out_state):
+        #    self.assertTrue(np.abs(exp_amplitude - out_amplitude) < 10 ** (-5))
+        self.assertTrue(np.isclose(input_vector, out_state).all())
 
     def test_mult_initialization_random_4qubtis(self):
         """
@@ -123,5 +128,249 @@ class TestCircuitCreation(TestCase):
         result = job.result()
 
         out_state = result.get_statevector(quantum_circuit)
-        for exp_amplitude, out_amplitude in zip(input_vector, out_state):
-            self.assertTrue(np.abs(exp_amplitude - out_amplitude) < 10 ** (-5))
+        # for exp_amplitude, out_amplitude in zip(input_vector, out_state):
+        #    self.assertTrue(np.abs(exp_amplitude - out_amplitude) < 10 ** (-5))
+        self.assertTrue(np.isclose(input_vector, out_state).all())
+
+    def test_phase_extraction(self):
+        """
+            Test phase extraction procedure for phase encoding
+        """
+        expected_phases = np.array([0.9553166181245093, 0.3962075716952029,
+                                    0.7297276562269663, 0.25268025514207865])
+
+        complex_vector = [complex(np.sqrt(0.01), np.sqrt(0.02)),
+                          complex(np.sqrt(0.4), np.sqrt(0.07)),
+                          complex(np.sqrt(0.1), np.sqrt(0.08)),
+                          complex(np.sqrt(0.3), np.sqrt(0.02))]
+
+        _, returned_phases = InitializerUniformlyRotation._extract_phase_from_complex(0, complex_vector)
+
+        # for phase_index in range(len(expected_phases)):
+        #    self.assertTrue(np.abs(expected_phases[phase_index] - returned_phases[phase_index]) < 10e-5)
+        self.assertTrue(np.isclose(expected_phases, returned_phases).all())
+
+    def test_phase_with_only_one_entry(self):
+        """
+            Testing phase calculation where the state vector has only one entry
+        """
+
+        expected_phases = [0.7853981633974483, 0.0, 0.0, 0.0]
+        input_vector = np.array([complex(np.sqrt(0.5), np.sqrt(0.5)), 0, 0, 0])
+
+        _, returned_phases = InitializerUniformlyRotation._extract_phase_from_complex(0, input_vector)
+
+        # for phase_index in range(len(expected_phases)):
+        #    self.assertTrue(np.abs(expected_phases[phase_index] - returned_phases[phase_index]) < 10e-5)
+        self.assertTrue(np.isclose(expected_phases, returned_phases).all())
+
+    def test_phase_angles_computation(self):
+        """
+            Testing phases calculations of the phase encoding procedure
+        """
+
+        complex_vector = [complex(np.sqrt(0.01), np.sqrt(0.02)),
+                          complex(np.sqrt(0.4), np.sqrt(0.07)),
+                          complex(np.sqrt(0.1), np.sqrt(0.08)),
+                          complex(np.sqrt(0.3), np.sqrt(0.02))]
+
+        phases_vector = np.array([0.9553166181245093,
+                                  0.3962075716952029,
+                                  0.7297276562269663,
+                                  0.25268025514207865])
+
+        expected_angles = np.array([-0.0922790696126668,
+                                    -0.27955452321465324,
+                                    -0.23852370054244385])
+
+        initializer = InitializerUniformlyRotation(complex_vector)
+        initializer._compute_phase_equalization_angles(phases_vector)
+        phases_angles = initializer._phases_tree
+
+        # for angles_indexes in range(len(phases_angles)):
+        #    self.assertTrue(np.abs(expected_angles[angles_indexes] - phases_angles[angles_indexes]) < 10e-5)
+        self.assertTrue(np.isclose(expected_angles, phases_angles).all())
+
+    def test_phase_angles_computations_with_one_entry(self):
+        """
+            Test phases calculations for feature vectors with only one entry
+        """
+        input_vector = [complex(np.sqrt(0.5), np.sqrt(0.5)), 0, 0, 0]
+        phases_vector = [0.7853981633974483, 0.0, 0.0, 0.0]
+
+        expected_angles = [-0.19634954084936207, -0.39269908169872414, 0.0]
+
+        initializer = InitializerUniformlyRotation(input_vector)
+        initializer._compute_phase_equalization_angles(phases_vector)
+        phases_angles = initializer._phases_tree
+
+        self.assertTrue(np.isclose(expected_angles, phases_angles).all())
+
+    def test_ur_intialization_with_phase_encoding(self):
+        """
+            Test phase Möttönen encoding with complex features.
+            Using a 2 qubit statevector as input
+        """
+
+        input_vector = [complex(np.sqrt(0.01), np.sqrt(0.02)),
+                        complex(np.sqrt(0.4), np.sqrt(0.07)),
+                        complex(np.sqrt(0.1), np.sqrt(0.08)),
+                        complex(np.sqrt(0.3), np.sqrt(0.02))]
+
+        quantum_register = QuantumRegister(2)
+        quantum_circuit = QuantumCircuit(quantum_register)
+        quantum_circuit.ur_initialize(input_vector, quantum_register)
+
+        backend_sim = Aer.backends('statevector_simulator')[0]
+        job = execute(quantum_circuit, backend_sim)
+        result = job.result()
+        out_state = result.get_statevector()
+
+        self.assertTrue(np.isclose(out_state, input_vector).all())
+
+    def test_ur_phase_encoding_initialization_with_negative_complex(self):
+        """
+            Test phase Möttönen encoding with complex features,
+            with negative valued imaginary components.
+            Using a 2 qubit statevector as input
+        """
+
+        input_vector = [complex(np.sqrt(0.01), -np.sqrt(0.02)),
+                        complex(np.sqrt(0.4), -np.sqrt(0.07)),
+                        complex(np.sqrt(0.1), -np.sqrt(0.08)),
+                        complex(np.sqrt(0.3), -np.sqrt(0.02))]
+
+        quantum_register = QuantumRegister(2)
+        quantum_circuit = QuantumCircuit(quantum_register)
+        quantum_circuit.ur_initialize(input_vector, quantum_register)
+
+        backend_sim = Aer.backends('statevector_simulator')[0]
+        job = execute(quantum_circuit, backend_sim)
+        result = job.result()
+        out_state = result.get_statevector()
+
+        self.assertTrue(np.isclose(out_state, input_vector).all())
+
+    def test_ur_phase_encoding_initialization_3qubit_with_negative_complex(self):
+        """
+            Test phase Möttönen encoding with complex features,
+            with negative valued imaginary components.
+            Using a 3 qubit statevector as input
+        """
+
+        input_vector = [complex(np.sqrt(0.01), -np.sqrt(0.02)),
+                        complex(np.sqrt(0.01), -np.sqrt(0.01)),
+                        complex(np.sqrt(0.01), -np.sqrt(0.01)),
+                        complex(np.sqrt(0.02), -np.sqrt(0.01)),
+                        complex(np.sqrt(0.05), -np.sqrt(0.05)),
+                        complex(np.sqrt(0.3), -np.sqrt(0.1)),
+                        complex(np.sqrt(0.2), -np.sqrt(0.1)),
+                        complex(np.sqrt(0.05), -np.sqrt(0.05))]
+
+        quantum_register = QuantumRegister(3)
+        quantum_circuit = QuantumCircuit(quantum_register)
+        quantum_circuit.ur_initialize(input_vector, quantum_register)
+
+        backend_sim = Aer.backends('statevector_simulator')[0]
+        job = execute(quantum_circuit, backend_sim)
+        result = job.result()
+        out_state = result.get_statevector()
+
+        self.assertTrue(np.isclose(out_state, input_vector).all())
+
+    def test_ur_phase_encoding_3qubit_with_negative_real(self):
+        """
+            Test phase Möttönen encoding with complex features,
+            with negative valued real components.
+            Using a 3 qubit statevector as input
+        """
+        input_vector = [complex(-np.sqrt(0.01), np.sqrt(0.02)),
+                        complex(-np.sqrt(0.01), np.sqrt(0.01)),
+                        complex(-np.sqrt(0.01), np.sqrt(0.01)),
+                        complex(-np.sqrt(0.02), np.sqrt(0.01)),
+                        complex(-np.sqrt(0.05), np.sqrt(0.05)),
+                        complex(-np.sqrt(0.3), np.sqrt(0.1)),
+                        complex(-np.sqrt(0.2), np.sqrt(0.1)),
+                        complex(-np.sqrt(0.05), np.sqrt(0.05))]
+
+        quantum_register = QuantumRegister(3)
+        quantum_circuit = QuantumCircuit(quantum_register)
+        quantum_circuit.ur_initialize(input_vector, quantum_register)
+
+        backend_sim = Aer.backends('statevector_simulator')[0]
+        job = execute(quantum_circuit, backend_sim)
+        result = job.result()
+        out_state = result.get_statevector()
+
+        self.assertTrue(np.isclose(out_state, input_vector).all())
+
+    def test_ur_phase_encoding_3qubit_alternating_signs(self):
+        """
+            Test phase Möttönen encoding with complex features,
+            with alternating negative values from real to complex.
+            Using a 3 qubit statevector as input
+        """
+        input_vector = [complex(-np.sqrt(0.01), np.sqrt(0.02)),
+                        complex(np.sqrt(0.01), -np.sqrt(0.01)),
+                        complex(-np.sqrt(0.01), np.sqrt(0.01)),
+                        complex(np.sqrt(0.02), -np.sqrt(0.01)),
+                        complex(-np.sqrt(0.05), np.sqrt(0.05)),
+                        complex(np.sqrt(0.3), -np.sqrt(0.1)),
+                        complex(-np.sqrt(0.2), np.sqrt(0.1)),
+                        complex(np.sqrt(0.05), -np.sqrt(0.05))]
+
+        quantum_register = QuantumRegister(3)
+        quantum_circuit = QuantumCircuit(quantum_register)
+        quantum_circuit.ur_initialize(input_vector, quantum_register)
+
+        backend_sim = Aer.backends('statevector_simulator')[0]
+        job = execute(quantum_circuit, backend_sim)
+        result = job.result()
+        out_state = result.get_statevector()
+
+        self.assertTrue(np.isclose(out_state, input_vector).all())
+
+    def test_ur_phase_encoding_3qubit_all_negative(self):
+        """
+            Test phase Möttönen encoding with complex features,
+            with all negative values from real to complex.
+            Using a 3 qubit statevector as input
+        """
+        input_vector = -np.array([complex(np.sqrt(0.01), np.sqrt(0.02)),
+                                  complex(np.sqrt(0.01), np.sqrt(0.01)),
+                                  complex(np.sqrt(0.01), np.sqrt(0.01)),
+                                  complex(np.sqrt(0.02), np.sqrt(0.01)),
+                                  complex(np.sqrt(0.05), np.sqrt(0.05)),
+                                  complex(np.sqrt(0.3), np.sqrt(0.1)),
+                                  complex(np.sqrt(0.2), np.sqrt(0.1)),
+                                  complex(np.sqrt(0.05), np.sqrt(0.05))])
+
+        quantum_register = QuantumRegister(3)
+        quantum_circuit = QuantumCircuit(quantum_register)
+        quantum_circuit.ur_initialize(input_vector, quantum_register)
+
+        backend_sim = Aer.backends('statevector_simulator')[0]
+        job = execute(quantum_circuit, backend_sim)
+        result = job.result()
+        out_state = result.get_statevector()
+
+        self.assertTrue(np.isclose(out_state, input_vector).all())
+
+    def test_ur_phase_encoding_3qubit_random_complex_entries(self):
+        """
+            Test phase Möttönen encoding with random complex features.
+            Using a 3 qubit statevector as input
+        """
+        input_vector = np.random.uniform(-2, 2, size=2**3) + 1j*np.random.uniform(-2, 2, size=2**3)
+        input_vector = input_vector / np.linalg.norm(input_vector)
+
+        quantum_register = QuantumRegister(3)
+        quantum_circuit = QuantumCircuit(quantum_register)
+        quantum_circuit.ur_initialize(input_vector, quantum_register)
+
+        backend_sim = Aer.backends('statevector_simulator')[0]
+        job = execute(quantum_circuit, backend_sim)
+        result = job.result()
+        out_state = result.get_statevector()
+
+        self.assertTrue(np.isclose(out_state, input_vector).all())
