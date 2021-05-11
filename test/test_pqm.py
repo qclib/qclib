e7a3181f822998bd267153fe68acd1a0360e5505
Israel F. Araujo <israelferrazaraujo@hotmail.com>
2021-05-11 18:07:42 -0300

    Added test with quantum input pattern

--- test/test_pqm.py
+++ test/test_pqm.py
@@ -9,28 +9,44 @@ from qclib.util import get_counts
 class TestPQM(TestCase):
 
     def test_pqm(self):
-        # pqm memory data
-        data = [1, 0, 0, 0]
-        data = data / np.linalg.norm(data)
+        def run_pqm(is_classical_pattern):
+            # pqm memory data
+            data = [0, 1, 0, 0]
+            data = data / np.linalg.norm(data)
 
-        # initialize quantum registers
-        memory = QuantumRegister(2, 'm')
-        aux = QuantumRegister(1, 'c')
-        output = ClassicalRegister(1)
-        circ = QuantumCircuit(memory, aux, output)
+            # initialize quantum registers
+            memory = QuantumRegister(2, 'm')
+            aux = QuantumRegister(1, 'c')
+            output = ClassicalRegister(1)
+            circ = QuantumCircuit(memory, aux, output)
 
-        # initialize data
-        init_gate = initialize(data)
-        circ.append(init_gate, memory)
+            # initialize data
+            init_gate = initialize(data)
+            circ.append(init_gate, memory)
 
-        # run pqm recovery algorithm
-        bin_input = [0, 0]
-        pqm(circ, bin_input, memory, aux, is_classical_pattern=True)
+            # initialize input pattern
+            bin_input = [1, 0]
 
-        # measure output and verify results
-        circ.measure(aux, output)
-        counts = get_counts(circ)
+            if (is_classical_pattern):
+                pqm(circ, bin_input, memory, aux, is_classical_pattern=True) # run pqm recovery algorithm
+            else:
+                q_bin_input = QuantumRegister(2)
+                circ.add_register(q_bin_input)
 
-        self.assertTrue(counts['0'] / 1024 == 1)
+                # Pattern basis encoding
+                for k, b in enumerate(bin_input):
+                    if (b == 1):
+                        circ.x(q_bin_input[k])
+
+                pqm(circ, q_bin_input, memory, aux, is_classical_pattern=False) # run pqm recovery algorithm
+
+            # measure output and verify results
+            circ.measure(aux, output)
+            counts = get_counts(circ)
+
+            self.assertTrue(counts['0'] / 1024 == 1)
+
+        run_pqm(True)   # classical input pattern
+        run_pqm(False)  # quantum input pattern
 
 
