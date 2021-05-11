9b96df7195f779edf40ae945d457d53267773a2a
Israel F. Araujo <israelferrazaraujo@hotmail.com>
2021-05-11 17:31:13 -0300

    Optional input pattern register type 2

--- qclib/pqm.py
+++ qclib/pqm.py
@@ -14,12 +14,11 @@ def pqm(circuit, pattern, q_memory, q_auxiliary, is_classical_pattern=False):
         
     if (is_classical_pattern):          
         for k, q_m in enumerate(q_memory): # classical pattern register
-            if (pattern[k]==0):            
+            if (pattern[k]==1):            
                 circuit.x(q_m)
     else:
         for k, q_m in enumerate(q_memory): # quantum pattern register
             circuit.cx(pattern[k], q_m)    
-            circuit.x(q_m)
 
     for k, q_m in enumerate(q_memory):
         circuit.p(-np.pi / (2 * size), q_m)
@@ -29,11 +28,10 @@ def pqm(circuit, pattern, q_memory, q_auxiliary, is_classical_pattern=False):
     
     if (is_classical_pattern):
         for k, q_m in list(enumerate(q_memory))[::-1]: # classical pattern register
-            if (pattern[k]==0):            
+            if (pattern[k]==1):            
                 circuit.x(q_m)
     else:
         for k, q_m in list(enumerate(q_memory))[::-1]: # quantum pattern register
-            circuit.x(q_m)                 
-            circuit.cx(q_pattern[k], q_m)  
+            circuit.cx(pattern[k], q_m)  
 
     circuit.h(q_auxiliary)
