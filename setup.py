795ac2208b382953b9c257afe3de7ec988ef18f4
Adenilton Silva <adenilton.silva@ufrpe.br>
2021-06-04 16:43:45 -0300

    pipy

--- setup.py
+++ setup.py
@@ -1,2 +1,21 @@
-from setuptools import setup
-setup()
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="qclib",
+    version="0.0.3",
+    author="Adenilton Silva",
+    author_email="ajsilva@cin.ufpe.br",
+    description="A quantum computing library using qiskit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/qclib/qclib",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
