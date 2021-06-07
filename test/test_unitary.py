6f0692ba10e7bb69504091731e4bc8f404a24109
Adenilton Silva <ajsilva@cin.ufpe.br>
2021-06-07 18:17:39 -0300

    Update test_unitary.py

--- test/test_unitary.py
+++ test/test_unitary.py
@@ -3,7 +3,7 @@ from scipy.stats import unitary_group
 from qclib.unitary import unitary, _compute_gates, _qsd
 from qclib.util import get_state
 import numpy as np
-from scipy.linalg import block_diag
+import qiskit
 
 class TestUnitary(TestCase):
     def test_unitary_csd_2qubits(self):
@@ -19,11 +19,43 @@ class TestUnitary(TestCase):
         state = get_state(gate)
         self.assertTrue(np.allclose(u[:, 0], state))
 
-    def test_unitary_qsd_3qubits(self):
+
+
+    def test_unitary_qsd_4qubits(self):
         u = unitary_group.rvs(16)
         gate = unitary(u, 'qsd')
         state = get_state(gate)
         self.assertTrue(np.allclose(u[:, 0], state))
+
+        circuit = qiskit.QuantumCircuit(4)
+        circuit.x(0)
+        circuit.append(gate, circuit.qubits)
+        state = get_state(circuit)
+        self.assertTrue(np.allclose(u[:, 1], state))
+
+        circuit = qiskit.QuantumCircuit(4)
+        circuit.x(1)
+        circuit.append(gate, circuit.qubits)
+        state = get_state(circuit)
+        self.assertTrue(np.allclose(u[:, 2], state))
+
+        circuit = qiskit.QuantumCircuit(4)
+
+        circuit.x([0, 1, 2, 3])
+        circuit.append(gate, circuit.qubits)
+        state = get_state(circuit)
+        self.assertTrue(np.allclose(u[:, 15], state))
+
+    def test_unitary_qsd_5qubits(self):
+        u = unitary_group.rvs(16)
+        gate = unitary(u, 'qsd')
+        state = get_state(gate)
+        transpiled_circuit = qiskit.transpile(gate, basis_gates=['u', 'cx'])
+        n_cx = transpiled_circuit.count_ops()['cx']
+        self.assertTrue(n_cx <= 120)
+        self.assertTrue(np.allclose(u[:, 0], state))
+
+
 
     def test_compute_gates(self):
         gate1 = unitary_group.rvs(8)
@@ -35,17 +67,6 @@ class TestUnitary(TestCase):
         calc2 = V @ np.diag(d).conj().T @ W
         self.assertTrue(np.allclose(calc1, gate1))
         self.assertTrue(np.allclose(calc2, gate2))
-
-    def test_qsd(self):
-        gate1 = unitary_group.rvs(2)
-        gate2 = unitary_group.rvs(2)
-
-        d, V, W = compute_gates(gate1, gate2)
-
-        gate = _qsd(gate1, gate2)
-
-        state = get_state(gate)
-        print(state)
 
 
 
