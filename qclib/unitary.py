d55bd99a3eebd30cb41f84867c6c738865cb45e2
Adenilton Silva <ajsilva@cin.ufpe.br>
2021-06-07 16:47:40 -0300

    qsd decomposition

--- qclib/unitary.py
+++ qclib/unitary.py
@@ -10,7 +10,7 @@ import qiskit
 import numpy as np
 
 
-def _unitary(gate_list, n_qubits):
+def _unitary(gate_list, n_qubits, decomposition='csd'):
 
     if len(gate_list[0]) == 2:
         qubits = qiskit.QuantumRegister(n_qubits)
@@ -18,22 +18,29 @@ def _unitary(gate_list, n_qubits):
         circuit.uc(gate_list, qubits[1:], qubits[0])
         return circuit
 
-    left, mid, right = _multiplexed_csd(gate_list)
+    if decomposition == 'csd':
+        left, mid, right = _multiplexed_csd(gate_list)
 
-    gate_left = _unitary(left, n_qubits)
-    gate_right = _unitary(right, n_qubits)
+        gate_left = _unitary(left, n_qubits)
+        gate_right = _unitary(right, n_qubits)
 
-    qubits = qiskit.QuantumRegister(n_qubits)
-    circuit = qiskit.QuantumCircuit(qubits)
+        qubits = qiskit.QuantumRegister(n_qubits)
+        circuit = qiskit.QuantumCircuit(qubits)
 
-    circuit = circuit.compose(gate_left, qubits)
+        circuit = circuit.compose(gate_left, qubits)
 
-    target = int(n_qubits - np.log2(len(left)))
-    control = list(range(0, target)) + list(range(target + 1, n_qubits))
-    circuit.ucry(list(mid), control, target)
+        target = int(n_qubits - np.log2(len(left)))
+        control = list(range(0, target)) + list(range(target + 1, n_qubits))
+        circuit.ucry(list(mid), control, target)
 
-    circuit = circuit.compose(gate_right, qubits)
-    return circuit
+        circuit = circuit.compose(gate_right, qubits)
+        return circuit
+
+    # qsd
+    return _qsd(*gate_list)
+
+
+
 
 
 def _multiplexed_csd(gate_list):
@@ -51,14 +58,39 @@ def _multiplexed_csd(gate_list):
 
     return left, mid, right
 
+def _qsd(gate1, gate2):
+    n_qubits = int(np.log2(len(gate1))) + 1
 
-def unitary(gate):
+    list_d, gate_v, gate_w = _compute_gates(gate1, gate2)
+    left_gate = unitary(gate_w, 'qsd')
+    right_gate = unitary(gate_v, 'qsd')
+
+    qubits = qiskit.QuantumRegister(n_qubits)
+    circuit = qiskit.QuantumCircuit(qubits)
+
+    circuit = circuit.compose(left_gate, qubits[0:-1])
+    circuit.ucrz(list(-2*np.angle(list_d)), qubits[0:-1], qubits[-1])
+    circuit = circuit.compose(right_gate, qubits[0:-1])
+    return circuit
+
+
+def _compute_gates(gate1, gate2):
+
+    d_square, gate_v = np.linalg.eig(gate1 @ gate2.conj().T)
+    list_d = np.sqrt(d_square)
+    gate_d = np.diag(list_d)
+    gate_w = gate_d @ gate_v.conj().T @ gate2
+
+    return list_d, gate_v, gate_w
+
+
+def unitary(gate, decomposition='csd'):
     """
     Implements a generic quantum computation from a
     unitary matrix gate using the cosine sine decomposition.
     """
     size = len(gate)
-    if size > 2:
+    if size > 4:
         n_qubits = int(np.log2(size))
 
         qubits = qiskit.QuantumRegister(n_qubits)
@@ -67,8 +99,8 @@ def unitary(gate):
         right_gates, theta, left_gates = \
             sp.linalg.cossin(gate, size/2, size/2, separate=True)
 
-        gate_left = _unitary(list(left_gates), n_qubits)
-        gate_right = _unitary(list(right_gates), n_qubits)
+        gate_left = _unitary(list(left_gates), n_qubits, decomposition)
+        gate_right = _unitary(list(right_gates), n_qubits, decomposition)
 
         circuit = circuit.compose(gate_left, qubits)
         circuit.ucry(list(2*theta), list(range(n_qubits-1)), n_qubits-1)
@@ -76,7 +108,7 @@ def unitary(gate):
 
         return circuit
 
-    circuit = qiskit.QuantumCircuit(1)
+    circuit = qiskit.QuantumCircuit(np.log2(size))
     circuit.unitary(gate, circuit.qubits)
 
     return circuit
--- test/test_unitary.py
+++ test/test_unitary.py
@@ -1,8 +1,9 @@
 from unittest import TestCase
 from scipy.stats import unitary_group
-from qclib.unitary import unitary
+from qclib.unitary import unitary, _compute_gates, _qsd
 from qclib.util import get_state
 import numpy as np
+from scipy.linalg import block_diag
 
 class TestUnitary(TestCase):
     def test_unitary_csd_2qubits(self):
@@ -17,3 +18,35 @@ class TestUnitary(TestCase):
         gate = unitary(u)
         state = get_state(gate)
         self.assertTrue(np.allclose(u[:, 0], state))
+
+    def test_unitary_qsd_3qubits(self):
+        u = unitary_group.rvs(16)
+        gate = unitary(u, 'qsd')
+        state = get_state(gate)
+        self.assertTrue(np.allclose(u[:, 0], state))
+
+    def test_compute_gates(self):
+        gate1 = unitary_group.rvs(8)
+        gate2 = unitary_group.rvs(8)
+
+        d, V, W = _compute_gates(gate1, gate2)
+
+        calc1 = V @ np.diag(d) @ W
+        calc2 = V @ np.diag(d).conj().T @ W
+        self.assertTrue(np.allclose(calc1, gate1))
+        self.assertTrue(np.allclose(calc2, gate2))
+
+    def test_qsd(self):
+        gate1 = unitary_group.rvs(2)
+        gate2 = unitary_group.rvs(2)
+
+        d, V, W = compute_gates(gate1, gate2)
+
+        gate = _qsd(gate1, gate2)
+
+        state = get_state(gate)
+        print(state)
+
+
+
+
