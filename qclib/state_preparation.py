d88226479e8d0b9bf7550e20d9920d801767c594
Adenilton Silva <ajsilva@cin.ufpe.br>
2021-07-11 14:44:20 -0300

    sparse_state_preparation

--- qclib/state_preparation.py
+++ qclib/state_preparation.py
@@ -4,6 +4,7 @@ State preparation using Schmidt decomposition 	arXiv:1003.5760
 import numpy as np
 import qiskit
 from qclib.unitary import unitary
+from qclib.util import get_state
 
 
 def initialize(unit_vector):
@@ -44,3 +45,120 @@ def initialize(unit_vector):
     sp_circuit.compose(gate_v, A, inplace=True)
 
     return sp_circuit
+
+
+def pivoting(index_zero, index_nonzero, target_size, state=None):
+    """
+
+    Attributes
+    ----------
+    index_zero:
+    index_nonzero:
+    target_size:
+    state:
+
+    Returns:
+
+    """
+    n_qubits = len(index_zero)
+    target = qiskit.QuantumRegister(n_qubits - target_size, name='t')
+    remainder = qiskit.QuantumRegister(target_size, name='r')
+    # circuit = qiskit.QuantumCircuit(target, remainder)
+    circuit = qiskit.QuantumCircuit(remainder, target)
+
+    target_nonzero = index_nonzero[:n_qubits-target_size][::-1]
+    target_zero = index_zero[:n_qubits-target_size][::-1]
+    remainder_nonzero = index_nonzero[n_qubits-target_size:][::-1]
+    remainder_zero = index_zero[n_qubits-target_size:][::-1]
+
+    index_differ = 0
+    for index, value in enumerate(target_nonzero):
+        if target_nonzero[index] != target_zero[index]:
+            index_differ = index
+            break
+
+    for index, value in enumerate(target_nonzero):
+        if index_differ != index:
+            if target_nonzero[index] != target_zero[index]:
+                circuit.cx(target[index_differ], target[index])
+
+
+
+    for index, value  in enumerate(remainder_nonzero):
+        if remainder_nonzero[index] != remainder_zero[index]:
+            circuit.cx(target[index_differ], remainder[index])
+
+    for index, value in enumerate(remainder_zero):
+        if value == '0':
+            circuit.x(remainder[index])
+
+    circuit.mcx(remainder, target[index_differ])
+
+    for index, value in enumerate(remainder_zero):
+        if value == '0':
+            circuit.x(remainder[index])
+
+    next_state = None
+    if list(state):
+        # next_state should not be calculated using a quantum circuit
+        circ_next = initialize(state / np.linalg.norm(state))
+        circ_next.compose(circuit, circ_next.qubits, inplace=True)
+        next_state = get_state(circ_next)
+    return circuit, next_state
+
+
+def sparse_initialize(state):
+
+    n_qubits = np.log2(len(state))
+    n_qubits = int(n_qubits)
+
+    pivot_circuit = qiskit.QuantumCircuit(n_qubits)
+
+    non_zero = _count_nonzero(state)
+
+    target_size = np.log2(non_zero)
+    target_size = np.ceil(target_size)
+    target_size = int(target_size)
+
+    fim = _count_nonzero(state[:2**target_size])
+    # for k, value in enumerate(state):
+    #     if not np.isclose(value, 0.0) and k < target_size:
+    #         fim += 1
+
+    next_state = state.copy()
+    while fim != non_zero:
+        for index, value in enumerate(next_state):
+            if not np.isclose(value, 0):
+                continue
+
+            index_zero = index
+            break
+
+        for index2, value2 in enumerate(next_state[2**target_size:]):
+            if not np.isclose(value2, 0):
+                fim += 1
+                index_nonzero = index2 + 2**target_size
+
+                txt = "{0:0" + str(n_qubits) + "b}"
+                index_zero_bin = txt.format(index_zero)
+                index_nonzero_bin = txt.format(index_nonzero)
+                print('pivoting')
+                circ, next_state = pivoting(index_zero_bin, index_nonzero_bin, target_size, next_state)
+                next_state[next_state < 1e-3] = 0.0
+                pivot_circuit.compose(circ, pivot_circuit.qubits, inplace=True)
+                break
+
+    initialize_circ = initialize(next_state[:2**target_size])
+    sp_circuit = qiskit.QuantumCircuit(n_qubits)
+    sp_circuit.compose(initialize_circ, sp_circuit.qubits[:target_size], inplace=True)
+    sp_circuit.compose(pivot_circuit.reverse_ops(), sp_circuit.qubits, inplace=True)
+
+    return sp_circuit
+
+
+def _count_nonzero(state):
+    non_zero = 0
+    for k in state:
+        if not np.isclose(k, 0.0):
+            non_zero += 1
+    return non_zero
--- test/test_initialize.py
+++ test/test_initialize.py
@@ -1,7 +1,8 @@
 from unittest import TestCase
 import numpy as np
-from qclib.state_preparation import initialize
+from qclib.state_preparation import initialize, pivoting, sparse_initialize
 from qclib.util import get_state
+from qiskit import transpile
 
 
 class TestInitialize(TestCase):
@@ -14,3 +15,68 @@ class TestInitialize(TestCase):
         state = get_state(circ)
 
         self.assertTrue(np.allclose(a, state))
+
+    def test_pivoting_2nonzero(self):
+        vector = [np.sqrt(2)/np.sqrt(3), 0, 0, 0, 0, 0, 0, 1/np.sqrt(3), 0, 0, 0, 0, 0, 0, 0, 0]
+
+        circ = initialize(vector)
+        n_qubits = int(np.log2(len(vector)))
+        txt = "{0:0" + str(n_qubits) + "b}"
+        index_zero = txt.format(1)
+        index_nonzero = txt.format(7)
+
+        circuit, next_state = pivoting(index_zero, index_nonzero, 2, state=vector)
+        circ.compose(circuit, circ.qubits, inplace=True)
+        vector2 = get_state(circ)
+        self.assertTrue(np.isclose(vector2[0], vector[0]))
+        self.assertTrue(np.isclose(vector2[1], vector[7]))
+
+    def test_pivoting_3nonzero(self):
+        vector = [1/np.sqrt(3), 0, 0, 0, 0, 0, 0, 1/np.sqrt(3), 0, 0, 0, 0, 0, 0, 0, 1/np.sqrt(3)]
+
+        circ = initialize(vector)
+        n_qubits = int(np.log2(len(vector)))
+        txt = "{0:0" + str(n_qubits) + "b}"
+        index_zero = txt.format(1)
+        index_nonzero = txt.format(7)
+
+        circuit, next_state = pivoting(index_zero, index_nonzero, 2, state=vector)
+        circ.compose(circuit, circ.qubits, inplace=True)
+
+        index_zero = txt.format(2)
+        index_nonzero = txt.format(9)
+
+        circuit, next_state = pivoting(index_zero, index_nonzero, 2, state=next_state)
+        circ.compose(circuit, circ.qubits, inplace=True)
+
+        vector2 = get_state(circ)
+        self.assertTrue(np.isclose(vector2[0], vector[0]))
+        self.assertTrue(np.isclose(vector2[1], vector[7]))
+
+    def test_sparse_initialize(self):
+        s = 2
+        n = 4
+        vector = np.zeros(2**n)
+
+        for k in range(2**s):
+            index = np.random.randint(0, 2**n)
+            while vector[index] != 0.0:
+                index = np.random.randint(0, 2**n)
+            vector[index] = np.random.rand()# + np.random.rand() * 1j
+
+        vector = vector / np.linalg.norm(vector)
+
+        # vector = [1 / np.sqrt(4), 1 / np.sqrt(4), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 / np.sqrt(4), 0, 0, 1 / np.sqrt(4)]
+        circ = sparse_initialize(vector)
+        calc_vector = get_state(circ)
+        print(np.allclose(vector, calc_vector))
+
+        circt = transpile(circ, basis_gates=['u', 'cx'], optimization_level=3)
+        print(circt.count_ops())
+
+
+
+
+
+
+
