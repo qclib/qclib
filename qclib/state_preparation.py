fe88f38250729b7e9af552bdecbdef3c8da4942c
Adenilton Silva <ajsilva@cin.ufpe.br>
2021-07-13 18:16:34 -0300

    Update state_preparation.py

--- qclib/state_preparation.py
+++ qclib/state_preparation.py
@@ -15,6 +15,7 @@ def initialize(unit_vector):
 
     size = len(state)
     n_qubits = np.log2(size)
+
     r = n_qubits % 2
 
     state.shape = (int(2**(n_qubits//2)), int(2**(n_qubits//2 + r)))
@@ -60,19 +61,9 @@ def pivoting(index_zero, index_nonzero, target_size, state=None):
     Returns:
 
     """
-    # index_nonzero = index_nonzero[::-1]
-    # index_zero = index_zero[::-1]
-    n_qubits = len(index_zero)
-    target = qiskit.QuantumRegister(n_qubits - target_size, name='t')
-    remainder = qiskit.QuantumRegister(target_size, name='r')
-    # circuit = qiskit.QuantumCircuit(target, remainder)
-    circuit = qiskit.QuantumCircuit(n_qubits)
-    # circuit = qiskit.QuantumCircuit(remainder, target)
 
-    target_nonzero = index_nonzero[:n_qubits-target_size]
-    target_zero = index_zero[:n_qubits-target_size]
-    remainder_nonzero = index_nonzero[n_qubits-target_size:]
-    remainder_zero = index_zero[n_qubits-target_size:]
+    n_qubits = len(index_zero)
+    circuit = qiskit.QuantumCircuit(n_qubits)
 
     tg = list(range(n_qubits - target_size))
     rg = list(range(n_qubits - target_size, n_qubits))
@@ -84,13 +75,6 @@ def pivoting(index_zero, index_nonzero, target_size, state=None):
             ctrl_state = index_nonzero[k]
             break
 
-    # index_differ = 0
-    # for index, value in enumerate(target_nonzero):
-    #     if target_nonzero[index] != target_zero[index]:
-    #         index_differ = index
-    #         ctrl_state = target_nonzero[index_differ]
-    #         break
-
     target_cx = []
     for k in tg:
         if index_differ != k:
@@ -98,25 +82,10 @@ def pivoting(index_zero, index_nonzero, target_size, state=None):
                 circuit.cx(index_differ, k, ctrl_state=ctrl_state)
                 target_cx.append(k)
 
-    # target_cx = []
-    # for index, value in enumerate(target_nonzero):
-    #     if index_differ != index:
-    #         if target_nonzero[index] != target_zero[index]:
-    #             circuit.cx(target[index_differ], target[index], ctrl_state=ctrl_state)
-    #             target_cx.append(index)
-
-    target_remainder = []
     for k in rg:
         if index_nonzero[k] != index_zero[k]:
             circuit.cx(index_differ, k, ctrl_state=ctrl_state)
-            # target_remainder.append(k)
             target_cx.append(k)
-
-    # target_remainder = []
-    # for index, value  in enumerate(remainder_nonzero):
-    #     if remainder_nonzero[index] != remainder_zero[index]:
-    #         circuit.cx(target[index_differ], remainder[index], ctrl_state=ctrl_state)
-    #         target_remainder.append(index)
 
     tab = {'0':'1', '1':'0'}
 
@@ -124,25 +93,14 @@ def pivoting(index_zero, index_nonzero, target_size, state=None):
         if index_zero[k] == '0':
             circuit.x(k)
 
-    # for index, value in enumerate(remainder_zero):
-    #     if value == '0':
-    #         circuit.x(remainder[index])
-
     circuit.mcx(rg, index_differ)
 
     for k in rg:
         if index_zero[k] == '0':
             circuit.x(k)
 
-    # for index, value in enumerate(remainder_zero):
-    #     if value == '0':
-    #         circuit.x(remainder[index])
-
-
     new_state = {}
     for index, value in state.items():
-        if index == '1011':
-            print(1)
 
         if index[index_differ] == ctrl_state:
 
@@ -166,6 +124,22 @@ def pivoting(index_zero, index_nonzero, target_size, state=None):
 
 
 def sparse_initialize(state):
+    """ Create circuit to initialize a sparse quantum state arXiv:2006.00016
+
+    For instance, to initialize the state a|001>+b|100>
+        $ state = {'001': a, '100': b}
+        $ circuit = sparse_initialize(state)
+
+    Parameters
+    ----------
+    state: dict of {str:int}
+        A unit vector representing a quantum state. Keys are binary strings and values are amplitudes.
+
+    Returns
+    -------
+    sp_circuit: QuantumCircuit
+        QuantumCircuit to initialize the state
+    """
     key, _ = list(state.items())[0]
     n_qubits = len(key)
     n_qubits = int(n_qubits)
@@ -185,19 +159,24 @@ def sparse_initialize(state):
 
         index_zero = _get_index_zero(n_qubits, non_zero, next_state)
 
-        print('pivoting')
         circ, next_state = pivoting(index_zero, index_nonzero, target_size, next_state)
         pivot_circuit.compose(circ, pivot_circuit.qubits, inplace=True)
 
-        index_nonzero = _get_index_nz(next_state, target_size)
+        index_nonzero = _get_index_nz(next_state, n_qubits - target_size)
 
     dense_state = np.zeros(2**(target_size))
     for key, value in next_state.items():
+
         dense_state[int(key, 2)] = value
-    initialize_circ = initialize(dense_state)
+    if non_zero <= 2:
+        initialize_circ = qiskit.QuantumCircuit(1)
+        initialize_circ.initialize(dense_state)
+    else:
+        initialize_circ = initialize(dense_state)
     sp_circuit = qiskit.QuantumCircuit(n_qubits)
     sp_circuit.compose(initialize_circ, sp_circuit.qubits[:target_size], inplace=True)
-    sp_circuit.compose(pivot_circuit.reverse_ops(), sp_circuit.qubits, inplace=True)
+    sp_circuit.barrier()
+    sp_circuit.compose(pivot_circuit.reverse_bits().reverse_ops(), sp_circuit.qubits, inplace=True)
 
     return sp_circuit
 
@@ -216,7 +195,7 @@ def _get_index_zero(n_qubits, non_zero, state):
 def _get_index_nz(state, target_size):
     index_nonzero = None
     for index, _ in state.items():
-        if index[:target_size] != target_size * '0':
+        if index[:target_size] != (target_size) * '0':
             index_nonzero = index
             break
     return index_nonzero
