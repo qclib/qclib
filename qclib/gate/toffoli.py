3a6dcc075e629e0cf83e24c6ba387526dc9c42fa
Adenilton Silva <ajsilva@cin.ufpe.br>
2021-10-18 16:31:50 -0300

    Update toffoli.py

--- qclib/gate/toffoli.py
+++ qclib/gate/toffoli.py
@@ -6,7 +6,7 @@ import qiskit
 import numpy as np
 
 
-def toffoli(qcirc: qiskit.QuantumCircuit, controls: list, targ: int, first=True):
+def toffoli(qcirc: qiskit.QuantumCircuit, controls: list, targ: int):
     """
 
     Parameters
@@ -20,20 +20,78 @@ def toffoli(qcirc: qiskit.QuantumCircuit, controls: list, targ: int, first=True)
     -------
 
     """
-    n_controls = len(controls)
 
-    for k in range(n_controls-1):
-        _coluna(qcirc, controls[n_controls-k:] + [targ],
-                controls[-1-k],
-                _Flags(False, False, first))
+    n_qubits = len(controls) + 1
+    gate_circuit = qiskit.QuantumCircuit(n_qubits, name="T" + str(targ))
+    coef = _coefficients(n_qubits)
 
-    _coluna(qcirc, controls[1:] + [targ], controls[0], _Flags(True, not first, first))
+    c1c2(coef, n_qubits, gate_circuit)
+    c3(coef, n_qubits, gate_circuit)
 
-    for k in range(n_controls-2, -1, -1):
-        _coluna(qcirc, controls[n_controls-k:] + [targ], controls[-1-k], _Flags(False, True, first))
+    c1c2(coef[:-1,1:-1], n_qubits-1, gate_circuit, False)
+    c3(coef[:-1,1:-1], n_qubits-1, gate_circuit)
 
-    if first:
-        toffoli(qcirc, controls[:-1], controls[-1], first=False)
+    qcirc.compose(gate_circuit, controls + [targ], inplace=True)
+
+
+def c3(coef, n_qubits, qcirc):
+    for k in range(n_qubits - 2, 1, -1):
+        line = k
+        column = n_qubits - 1
+        while line <= n_qubits - 2:
+            qcirc.crx(np.pi / coef[line, column], (column % (n_qubits - 1) + 1), line + 1)
+            line = line + 1
+            column = column + 1
+    for k in range(1, n_qubits - 1):
+        qcirc.crx(-np.pi / 2, k, k + 1)
+
+
+def c1c2(coef, n_qubits, qcirc, first=True):
+    if not first:
+        for k in range(n_qubits-1):
+            coef[k, n_qubits-2] = - coef[k, n_qubits-2]
+
+    for k in range(n_qubits - 1):
+        line = n_qubits - 2
+        column = k
+        while line >= 0 and column >= 0 and coef[line, column] != 0:
+            control = n_qubits - 2 - column
+            targ = line + 1
+
+            if first and line == n_qubits-2:
+                csqgate = xgate(coef, column, line)
+                qcirc.compose(csqgate, qubits=[control, targ], inplace=True)
+
+            else:
+                qcirc.crx(np.pi / coef[line, column], control, targ)
+
+            line = line - 1
+            column = column - 1
+
+
+
+    for k in range(n_qubits - 3, -1, -1):
+        column = n_qubits - 2
+        line = k
+        while line >= 0 and column >= 0 and coef[line, column] != 0:
+            qcirc.crx(np.pi / coef[line, column], n_qubits - 2 - column, line + 1)
+            line = line - 1
+            column = column - 1
+
+
+def xgate(coef, column, line):
+    param = np.abs( 1 / coef[line, column])
+    plus = (1 / np.sqrt(2)) * np.array([[1], [1]])
+    minus = (1 / np.sqrt(2)) * np.array([[1], [-1]])
+
+    gate = np.power(1 + 0j, 1 / param) * plus @ plus.T + \
+           np.power(-1 + 0j, 1 / param) * minus @ minus.T
+
+    sqgate = qiskit.QuantumCircuit(1, name='X^1/' + str(coef[line, column]))
+    sqgate.unitary(gate, 0)
+    csqgate = sqgate.control(1)
+
+    return csqgate
 
 
 class _Flags(NamedTuple):
@@ -79,16 +137,27 @@ def _coluna(qcirc, targs, control, flags):
     return qcirc
 
 
+
+
 def _coefficients(n_qubits):
     coef = np.zeros((n_qubits - 1, 2 * n_qubits - 3))
+
     for i in range(0, n_qubits - 2):
         one_coef = 2 ** (i+1)
+
         for j in range(n_qubits - 2, -1, -1):
             coef[j, i] = one_coef
             coef[j, (-i + 2 * n_qubits - 4)] = -one_coef
             one_coef = one_coef // 2
+
             if one_coef < 2:
                 break
+
     for k in range(n_qubits - 1):
         coef[k][n_qubits - 2] = 2 ** k
+
     return coef
+
+if __name__ == '__main__':
+    qcirc = qiskit.QuantumCircuit(6)
+    toffoli(qcirc, [5,4,3,2,1], 0)
