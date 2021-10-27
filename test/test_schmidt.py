87654a2027a95c198eccdefa41a2cb9f3932a078
israelferrazaraujo <israelferrazaraujo@hotmail.com>
2021-10-27 05:51:04 -0300

    Measurement test

--- test/test_schmidt.py
+++ test/test_schmidt.py
@@ -18,19 +18,41 @@ Tests for the schmidt.py module.
 
 from unittest import TestCase
 import numpy as np
+from qiskit import ClassicalRegister, execute, Aer
 from qclib.state_preparation.schmidt import initialize
 from qclib.util import get_state
 
 # pylint: disable=missing-function-docstring
 # pylint: disable=missing-class-docstring
 
-class TestInitialize(TestCase):
+class TestSchmidt(TestCase):
     @staticmethod
     def mae(state, ideal):
         """
         Mean Absolute Error
         """
         return np.sum(np.abs(state-ideal))/len(ideal)
+
+    @staticmethod
+    def get_counts(circuit):
+        n_qubits = circuit.num_qubits
+        classical_reg = ClassicalRegister(n_qubits)
+        circuit.add_register(classical_reg)
+        circuit.measure(list(range(n_qubits)), classical_reg)
+
+        backend = Aer.get_backend('qasm_simulator')
+        counts = execute(circuit, backend, shots=8192).result().get_counts()
+
+        counts_with_zeros = {}
+        for i in range(2**n_qubits):
+            pattern = '{:0{}b}'.format(i, n_qubits)
+            if pattern in counts:
+                counts_with_zeros[pattern] = counts[pattern]
+            else:
+                counts_with_zeros[pattern] = 0.0
+
+        sum_values = sum(counts.values())
+        return [ value/sum_values for (key, value) in counts_with_zeros.items() ]
 
     def _test_initialize_rank(self, rank, max_mae=10**-17):
         state_vector = np.random.rand(32) + np.random.rand(32) * 1j
@@ -40,7 +62,7 @@ class TestInitialize(TestCase):
 
         state = get_state(circuit)
 
-        self.assertTrue(TestInitialize.mae(state,state_vector) < max_mae)
+        self.assertTrue(TestSchmidt.mae(state,state_vector) < max_mae)
 
     def test_initialize_full_rank(self):
         state_vector = np.random.rand(32) + np.random.rand(32) * 1j
@@ -51,6 +73,17 @@ class TestInitialize(TestCase):
         state = get_state(circuit)
 
         self.assertTrue(np.allclose(state_vector, state))
+
+    def test_measurement_full_rank(self):
+        state_vector = np.random.rand(32) + np.random.rand(32) * 1j
+        state_vector = state_vector / np.linalg.norm(state_vector)
+
+        circuit = initialize(state_vector)
+
+        state = TestSchmidt.get_counts(circuit)
+        
+        self.assertTrue(np.allclose( np.power(np.abs(state_vector),2), state,
+                        rtol=1e-01, atol=0.005))
 
     def test_initialize_rank_5(self):
         state_vector = np.random.rand(32) + np.random.rand(32) * 1j
