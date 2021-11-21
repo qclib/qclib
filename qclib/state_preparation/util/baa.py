f6d8cc968cf82af1dc83995226c4204b7fdd5506
Israel F. Araujo <israelferrazaraujo@hotmail.com>
2021-11-21 16:05:07 -0300

    Routines were rewritten.
    Added two new parameters to 'adaptive_approximation': 'strategy' and 'max_combination_size'. The first allows choosing between 'brute_force' and 'greedy' strategies for the tree search algorithm. The second allows you to limit the size of the combination between the qubits of the entangled states.

--- qclib/state_preparation/util/baa.py
+++ qclib/state_preparation/util/baa.py
@@ -17,62 +17,212 @@ Bounded Approximation Algorithm.
 https://arxiv.org/abs/2111.03132
 """
 
-from typing import List, Tuple, Optional
-import itertools
+from dataclasses import dataclass
+from itertools import combinations
+from typing import List
 import numpy as np
 
 # pylint: disable=missing-function-docstring
 # pylint: disable=missing-class-docstring
 
-class Split:
-    fidelity_loss: float
-    subsystem: Optional[Tuple[int, ...]]
+def adaptive_approximation(state_vector, max_fidelity_loss,
+                            strategy='brute_force', max_combination_size=0):
+    """
+    It reduces the entanglement of the given state, producing an approximation
+    to reduce the complexity of the quantum circuit needed to prepare it.
+    `https://arxiv.org/abs/2111.03132`_.
+    Args:
+        state_vector (list):
+            A state vector to be approximated by a less complex state.
+        max_fidelity_loss (float):
+            Maximum fidelity loss allowed to the approximated state.
+        strategy (string):
+            Method to search for the best approximation ('brute_force' or 'greedy').
+            For states larger than 2**8, the greedy strategy should preferably be used.
+            Default is strategy='brute_force'.
+        max_combination_size (int):
+            Maximum size of the combination ``C(n_qubits, max_combination_size)``
+            between the qubits of an entangled subsystem of length ``n_qubits`` to
+            produce the possible bipartitions
+            (1 <= ``max_combination_size`` <= ``n_qubits``//2).
+            For example, if ``max_combination_size``==1, there will be ``n_qubits``
+            bipartitions between 1 and ``n_qubits``-1 qubits.
+            The default value is 0 (the size will be maximum for each level).
 
-    def __init__(self, subsystem: Optional[Tuple[int, ...]], fidelity_loss: float):
-        self.subsystem = subsystem
-        self.fidelity_loss = fidelity_loss
+    Returns:
+        Node: a node with the data required to build the quantum circuit.
+    """
+    n_qubits = int(np.log2(len(state_vector)))
+    qubits = [list(range(n_qubits))]
+    vectors = [state_vector]
 
-    def __str__(self):
-        return f'{type(self).__name__}|{self.subsystem}|'
+    root_node = Node(0, 0, 0.0, 0.0, vectors, qubits, [])
+    _build_approximation_tree(root_node, max_fidelity_loss, strategy, max_combination_size)
 
-    def __repr__(self):
-        return str(self)
+    leafs = []
+    _search_leafs(root_node, leafs)
 
-class Node:
-    split_program: Tuple[Split, ...]
-    vectors: List[np.ndarray]
-    cnot_saving: int
-    fidelity_loss: float
+    best_node = _search_best(leafs)
 
-    def __init__(self, split_program: Tuple[Split, ...], vectors: List[np.ndarray],
-                    fidelity_loss: float, cnot_saving: int):
-        self.fidelity_loss = fidelity_loss
-        self.cnot_saving = cnot_saving
-        self.vectors = vectors
-        self.split_program = split_program
-
-    def __getitem__(self, item):
-        data = [self.split_program, self.vectors, self.fidelity_loss, self.cnot_saving]
-        return data[item]
-
-    def __iter__(self):
-        data = [self.split_program, self.vectors, self.fidelity_loss, self.cnot_saving]
-        return iter(data)
-
-    def __str__(self):
-        return f'Node{(self.split_program, self.fidelity_loss, self.cnot_saving, self.vectors)}'
-
-    def __repr__(self):
-        return str(self)
-
-def adaptive_approximation(vector: np.ndarray, max_fidelity_loss: float) -> Optional[Node]:
-    best_node: Optional[Node] = _search_best_node([vector], 0, 0.0, max_fidelity_loss)
     return best_node
 
 
 
 
-def _sp_cnots(n_qubits) -> int:
+
+@dataclass
+class Node:
+    """
+    Tree node used in _approximation_tree function
+    """
+    node_saved_cnots: int
+    total_saved_cnots: int
+
+    node_fidelity_loss: float
+    total_fidelity_loss: float
+
+    vectors: List[List[complex]]
+    qubits: List[List[int]]
+
+    nodes: List['Node']
+
+    def __str__(self):
+        return f'total_saved_cnots:{self.total_saved_cnots}\n' + \
+               f'total_fidelity_loss:{self.total_fidelity_loss}\n' + \
+               f'len(subsystems):{len(self.qubits)}\n'
+
+def _build_approximation_tree(node, max_fidelity_loss, strategy='brute_force', max_k=0):
+    # Ignore the completely disentangled qubits.
+    entangled_qubits_list  = [i for i in node.qubits if len(i) > 1]
+    entangled_vectors_list = [i for i in node.vectors if len(i) > 2]
+
+    for i, entangled_qubits in enumerate(entangled_qubits_list):
+        entangled_vector = entangled_vectors_list[i]
+
+        if not 1 <= max_k <= len(entangled_qubits)//2:
+            max_k = len(entangled_qubits)//2
+
+        for k in range(1, max_k+1):
+            # Disentangles each possible bipartion from entangled_qubits.
+            for qubits_to_disentangle in combinations(entangled_qubits, k):
+                # Computes the two state vectors after disentangling "qubits_to_disentangle".
+                node_fidelity_loss, subsystem1, subsystem2 = \
+                    _compute_schmidt(entangled_vector, entangled_qubits, qubits_to_disentangle)
+
+                total_fidelity_loss = 1 - (1 - node_fidelity_loss) * \
+                                          (1 - node.total_fidelity_loss)
+
+                # Recursion should not continue in this branch if "total_fidelity_loss" has
+                # reached "max_fidelity_loss". The leaf corresponds to the node of best
+                # approximation of "max_fidelity_loss" on the branch.
+                if total_fidelity_loss <= max_fidelity_loss:
+                    index = node.qubits.index(entangled_qubits)
+                    new_node = _create_node(node, index, qubits_to_disentangle,
+                                                node_fidelity_loss, subsystem1, subsystem2)
+                    # Create one node for each bipartition.
+                    node.nodes.append(new_node)
+
+    if len(node.nodes) > 0:  # If it is not the end of the recursion,
+        node.vectors.clear() # clear vectors and qubits to save memory.
+        node.qubits.clear()  # This information is no longer needed from this point
+                             # on (but may be needed in the future).
+    if strategy == 'greedy' and len(node.nodes) > 0:
+        # Locally optimal choice at each stage.
+        node.nodes = [_search_best(node.nodes)]
+
+    for new_node in node.nodes:
+        # call _build_approximation_tree recurrently for each new node.
+        _build_approximation_tree(new_node, max_fidelity_loss, strategy, max_k)
+
+
+
+
+def _compute_schmidt(state_vector, entangled_qubits, qubits_to_disentangle):
+    local_qubits_to_disentangle = []
+    # Maintains the relative position between the qubits of the two subsystems.
+    for qubit_to_disentangle in qubits_to_disentangle:
+        local_qubits_to_disentangle.append(sum(i < qubit_to_disentangle for i in entangled_qubits))
+
+    sep_matrix = _separation_matrix(state_vector, local_qubits_to_disentangle)
+    svd_u, svd_s, svd_v = np.linalg.svd(sep_matrix, full_matrices=False)
+
+    subsystem1_vector = svd_u[:, 0]
+    subsystem2_vector = svd_v.T[:, 0]
+
+    node_fidelity_loss = 1 - (svd_s ** 2)[0] # svd_s first coefficient.
+
+    return node_fidelity_loss, subsystem1_vector, subsystem2_vector
+
+def _create_node(node, index, qubits_to_disentangle, node_fidelity_loss,
+                                                subsystem1_vector, subsystem2_vector):
+    total_fidelity_loss = 1 - (1 - node_fidelity_loss) * (1 - node.total_fidelity_loss)
+
+    vectors = node.vectors.copy()
+    qubits  = node.qubits.copy()
+
+    entangled_vector = vectors.pop(index)
+    entangled_qubits = qubits.pop(index)
+
+    subsystem1_qubits = list(set(entangled_qubits).difference(set(qubits_to_disentangle)))
+    subsystem2_qubits = qubits_to_disentangle
+
+    node_saved_cnots = \
+        _count_saved_cnots(entangled_vector, subsystem1_vector, subsystem2_vector)
+
+    total_saved_cnots = node.total_saved_cnots + node_saved_cnots
+
+    vectors.append(subsystem1_vector)
+    qubits.append(subsystem1_qubits)
+
+    vectors.append(subsystem2_vector)
+    qubits.append(subsystem2_qubits)
+
+    return Node(node_saved_cnots, total_saved_cnots, node_fidelity_loss,
+                                total_fidelity_loss, vectors, qubits, [])
+
+def _search_leafs(node, leafs):
+    # It returns the leaves of the tree. These nodes are the ones with
+    # total_fidelity_loss closest to max_fidelity_loss for each branch.
+    if len(node.nodes) == 0:
+        leafs.append(node)
+    else:
+        for child in node.nodes:
+            _search_leafs(child, leafs)
+
+def _search_best(nodes):
+    # Nodes with the greatest reduction in the number of CNOTs.
+    # There may be several with the same number.
+    max_total_saved_cnots = max(nodes, key=lambda n: n.total_saved_cnots).total_saved_cnots
+    max_saved_cnots_nodes = [node for node in nodes
+                                if node.total_saved_cnots == max_total_saved_cnots]
+    # Node with the lowest fidelity loss among the nodes with
+    # the highest reduction in the number of CNOTs.
+    return min(max_saved_cnots_nodes, key=lambda n: n.total_fidelity_loss)
+
+def _separation_matrix(vector, subsystem2):
+    n_qubits =  int(np.ceil(np.log2(vector.shape[0])))
+    subsystem1 = list(set(range(n_qubits)).difference(set(subsystem2)))
+
+    new_shape = (2 ** len(subsystem1), 2 ** len(subsystem2))
+
+    sep_matrix = np.zeros(shape=new_shape, dtype=complex)
+
+    for j, amp in enumerate(vector):
+        current = f'{j:b}'.zfill(n_qubits)
+        idx2 = ''.join([c for i, c in enumerate(current) if i in subsystem2])
+        idx1 = ''.join([c for i, c in enumerate(current) if i in subsystem1])
+        sep_matrix[int(idx1, 2), int(idx2, 2)] = amp
+
+    return sep_matrix
+
+def _count_saved_cnots(state_vector, entangled_vector, disentangled_vector):
+    cnots_phase_3 = _cnots(_to_qubits(entangled_vector.shape[0]))
+    cnots_phase_4 = _cnots(_to_qubits(disentangled_vector.shape[0]))
+    cnots_originally = _cnots(_to_qubits(state_vector.shape[0]))
+
+    return cnots_originally - cnots_phase_3 - cnots_phase_4
+
+def _cnots(n_qubits):
     if n_qubits == 1:
         return 0
     if n_qubits == 2:
@@ -89,115 +239,3 @@ def _sp_cnots(n_qubits) -> int:
 
 def _to_qubits(n_state_vector):
     return int(np.ceil(np.log2(n_state_vector)))
-
-def _get_complementary_subsystem(subsystem: Tuple[int, ...], num_qubits: int):
-    subsystem_c = tuple(set(range(num_qubits)).difference(set(subsystem)))
-    return subsystem_c
-
-def _get_separation_matrix(vector: np.ndarray, subsystem_2: Tuple[int, ...]):
-    n_qubits =  int(np.ceil(np.log2(vector.shape[0])))
-    subsystem_1 = _get_complementary_subsystem(subsystem_2, n_qubits)
-
-    new_shape = (2 ** len(subsystem_1), 2 ** len(subsystem_2))
-    sep_matrix = np.zeros(shape=new_shape, dtype=complex)
-
-    for i, amp in enumerate(vector):
-        current = f'{i:b}'.zfill(n_qubits)
-        number_2 = ''.join([c for i, c in enumerate(current) if i in subsystem_2])
-        number_1 = ''.join([c for i, c in enumerate(current) if i in subsystem_1])
-        sep_matrix[int(number_1, 2), int(number_2, 2)] = amp
-
-    return sep_matrix
-
-def _get_bipartite_systems(vector: np.ndarray) -> List[Tuple[int, ...]]:
-    # Bundle the state vector into a qutip Qobj
-    num_qb = int(np.ceil(np.log2(vector.shape[0])))
-
-    size_biggest_coef = []
-    # This loop looks at the principal subsystem size: starting from 1 to at most
-    # the half of the system (+1 to have in included).
-    # The reason is that HS decomposition says that the EVs are the same for the 'other' system.
-    for size in range(1, num_qb // 2 + 1):
-        sub_systems = itertools.combinations(range(num_qb), size)
-        size_biggest_coef += sub_systems
-    return size_biggest_coef
-
-def _compute_schmidt_states(vector, split, new_vectors):
-    sep_matrix = _get_separation_matrix(vector, split.subsystem)
-    svd_u, svd_s, svd_v = np.linalg.svd(sep_matrix, full_matrices=False)
-    vector_1 = svd_u[:, 0]
-    vector_2 = svd_v.T[:, 0]
-    new_vectors.append(vector_1)
-    new_vectors.append(vector_2)
-    cnots_phase_3 = _sp_cnots(_to_qubits(vector_1.shape[0]))
-    cnots_phase_4 = _sp_cnots(_to_qubits(vector_2.shape[0]))
-    cnots_originally = _sp_cnots(_to_qubits(vector.shape[0]))
-    saved_cnots = cnots_originally - cnots_phase_3 - cnots_phase_4
-    split.fidelity_loss = 1 - (svd_s ** 2)[0]
-
-    return saved_cnots
-
-def _get_nodes_from_activations(vectors: List[np.ndarray],
-                                subsystem_list: List[List[Tuple[int, ...]]],
-                                activations: np.ndarray) -> List[Node]:
-    result = []
-    # This step is the main step, I need to explain this a bit better!
-    all_paths = [[Split(dd, -1.0) for dd in d] if a == 1 else [Split(None, 0.0)] for d, a in
-                 zip(subsystem_list, activations)]
-
-    # The product makes all possible cross-product combinations as given above.
-    # Need to explain this better!
-    for split_program in itertools.product(*all_paths):
-        # apply the split program to the vectors and generate the new children
-        new_vectors = []
-        new_fidelity = 1.0
-        saved_cnots = 0
-        split: Split
-        for vector, split in zip(vectors, split_program):
-            if split.subsystem is None:
-                new_vectors.append(vector)
-            else:
-                saved_cnots += _compute_schmidt_states(vector, split, new_vectors)
-                new_fidelity *= 1 - split.fidelity_loss
-        result.append(Node(split_program, new_vectors, 1 - np.around(new_fidelity, 6),
-                                                                            saved_cnots))
-    return result
-
-def _generate_subsystem_partitions(vectors: List[np.ndarray], no_branching=False):
-    subsystem_list: List[List[Tuple[int], ...]] = \
-        [_get_bipartite_systems(vector) for vector in vectors]
-
-    result = []
-    activations = np.zeros(len(vectors))
-    activations[0] = 1
-    if no_branching:
-        result += _get_nodes_from_activations(vectors,
-                    [[d[0]] for d in subsystem_list if len(d) > 0], activations)
-    else:
-        for activations in [np.roll(activations, i) for i in range(len(vectors))]:
-            if set(activations) == {0}:
-                continue
-            result += _get_nodes_from_activations(vectors, subsystem_list, activations)
-
-    return result
-
-def _search_best_node(vectors: List[np.ndarray], running_cnot_saving: int,
-                        running_fidelity_loss: float, max_fidelity_loss: float) -> Optional[Node]:
-    data: List[Node] = _generate_subsystem_partitions(vectors)
-    possible_data: List[Node] = [d for d in data
-                                    if 1 - (1 - d.fidelity_loss) * (1 - running_fidelity_loss)
-                                        <= max_fidelity_loss]
-    better = [_search_best_node(p.vectors, running_cnot_saving + p.cnot_saving,
-                                running_fidelity_loss + p.fidelity_loss, max_fidelity_loss)
-                                for p in possible_data]
-    if len(better) == 0 or (set(better) == {None}):
-        if len(possible_data) == 0:
-            return None
-
-        best_possible = max(possible_data, key=lambda n: n.cnot_saving)
-        total_fidelity_loss = 1 - (1 - best_possible.fidelity_loss) * \
-                                    (1 - running_fidelity_loss)
-        total_cnot_savings = running_cnot_saving + best_possible.cnot_saving
-        return Node((), best_possible.vectors, total_fidelity_loss, total_cnot_savings)
-
-    return max([b for b in better if b is not None], key=lambda n: n.cnot_saving)
