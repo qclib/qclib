df51008d27f19027184816523f04efe9a326b592
Carsten Blank <blank@data-cybernetics.com>
2022-01-02 18:39:25 +0100

    I forgot to update the logic of selecting subsets of the qubits by using the rank (!= 0). Then the fix of is_leaf.

--- qclib/state_preparation/util/baa.py
+++ qclib/state_preparation/util/baa.py
@@ -130,7 +130,7 @@ class Node:
         True if the all vectors have reached an approximation assessment. There is no more
         decomposition/approximation possible. Therefore, the node is a leaf.
         """
-        return all(np.asarray(self.ranks) >= 1) or len(self.nodes) == 0
+        return all(np.asarray(self.ranks) >= 1)
 
     def num_qubits(self) -> int:
         """ Complete state number of qubits. """
@@ -156,11 +156,9 @@ class Node:
 def _build_approximation_tree(node, max_fidelity_loss, strategy='brute_force', max_k=0,
                                                                     use_low_rank=False):
     # Ignore the completely disentangled qubits.
-    entangled_qubits_list  = [i for i in node.qubits if len(i) > 1]
-    entangled_vectors_list = [i for i in node.vectors if len(i) > 2]
+    data = [(q, v) for q, v, k in zip(node.qubits, node.vectors, node.ranks) if k == 0]
 
-    for i, entangled_qubits in enumerate(entangled_qubits_list):
-        entangled_vector = entangled_vectors_list[i]
+    for entangled_qubits, entangled_vector in data:
 
         if not 1 <= max_k <= len(entangled_qubits)//2:
             max_k = len(entangled_qubits)//2
