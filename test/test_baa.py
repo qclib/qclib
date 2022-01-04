6f71590d980990f6098c1260988bef2351e10630
Carsten Blank <blank@data-cybernetics.com>
2022-01-04 21:53:20 +0100

    It is so muhc more readable when pandas does the formatting. If it is installed, please use it!

--- test/test_baa.py
+++ test/test_baa.py
@@ -294,9 +294,14 @@ class TestBaa(TestCase):
             print("[FAIL] real_approximation_calculation_pass must be true", file=sys.stderr)
             test_passed = False
 
-        with np.printoptions(precision=2, linewidth=1000, suppress=True, threshold=sys.maxsize,
-                             floatmode='fixed'):
-            print(test_data)
+        import importlib.util
+        pandas_loader = importlib.util.find_spec('pandas')
+        if pandas_loader is None:
+            with np.printoptions(precision=2, linewidth=1000, suppress=True, threshold=sys.maxsize, floatmode='fixed'):
+                print(test_data)
+        else:
+            import pandas as pd
+            print(pd.DataFrame(test_data, columns=self.columns).to_string())
 
         self.assertTrue(test_passed, 'The tests should all pass.')
 
