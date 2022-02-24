569e93f25e86ffe8a4d9d3bc7ee7fcd7423e8ba1
Adenilton Silva <ajsilva@cin.ufpe.br>
2022-02-23 21:36:33 -0300

    refactor mcgatee

--- qclib/gates/mc_gate.py
+++ qclib/gates/mc_gate.py
@@ -20,11 +20,12 @@ import numpy as np
 from collections import namedtuple
 
 
-def mc_gate(gate:np.ndarray, qcirc: qiskit.QuantumCircuit, controls: list, targ: int):
+def mc_gate(gate: np.ndarray, qcirc: qiskit.QuantumCircuit, controls: list, targ: int):
     """
 
     Parameters
     ----------
+    gate: 2 X 2 unitary gate
     qcirc: qiskit.QuantumCircuit
     controls: list of control qubits
     targ: target qubit
@@ -36,52 +37,37 @@ def mc_gate(gate:np.ndarray, qcirc: qiskit.QuantumCircuit, controls: list, targ:
 
     n_qubits = len(controls) + 1
     gate_circuit = qiskit.QuantumCircuit(n_qubits, name="T" + str(targ))
-    coef = _coefficients(n_qubits)
 
     c1c2(gate, n_qubits, gate_circuit)
-    _c3(gate, coef, n_qubits, gate_circuit)
+    _c3(gate, n_qubits, gate_circuit)
 
     c1c2(gate, n_qubits-1, gate_circuit, False)
-    _c3(gate, coef, n_qubits - 1, gate_circuit, False)
+    _c3(gate, n_qubits - 1, gate_circuit, False)
 
     qcirc.compose(gate_circuit, controls + [targ], inplace=True)
 
 
-def _c3(gate, coef, n_qubits, qcirc, first=True):
+def _c3(gate, n_qubits, qcirc, first=True):
     """ phase 2 """
-    for k in range(1, n_qubits-1):
-        line = k
-        column = n_qubits - 1
+    pairs = namedtuple("pairs", ["control", "target"])
 
-        while column < 2*n_qubits-3 and coef[line, column] != 0:
-            control = (column % (n_qubits - 1) + 1)
-            targ = line + 1
-            # -----
-            if first and targ == n_qubits-1:
-                csqgate = _xgate(gate, coef, column, line)
-                qcirc.compose(csqgate, qubits=[control, targ], inplace=True)
-            else:
-                qcirc.crx(np.pi / coef[line, column], control, targ)
+    qubit_pairs = [pairs(control, target) for target in range(n_qubits) for control in range(1, target)]
+    qubit_pairs.sort(key=lambda e: e.control + e.target, reverse=False)
 
-            line = line - 1
-            column = column + 1
+    for pair in qubit_pairs:
+        exponent = pair.target - pair.control
+        if pair.control == 0:
+            exponent = exponent - 1
+        param = 2 ** exponent
+        signal = -1
 
-    for k in range(n_qubits, 2*n_qubits-3):
-        line = n_qubits-2
-        column = k
-        control = column - line
-        while column< 2*n_qubits-3 and coef[line, column] != 0:
-            targ = line + 1
-            # -----
-            if first and targ == n_qubits - 1:
-                csqgate = _xgate(gate, coef, column, line)
-                qcirc.compose(csqgate, qubits=[control, targ], inplace=True)
-            else:
-                qcirc.crx(np.pi / coef[line, column], control, targ)
+        if pair.target == n_qubits - 1 and first:
 
-            line = line - 1
-            column = column + 1
-            control = control + 1
+            csqgate = _gate_u(gate, param, signal)
+            qcirc.compose(csqgate, qubits=[pair.control, pair.target], inplace=True)
+        else:
+            qcirc.crx(signal * np.pi / param, pair.control, pair.target)
+
 
 def c1c2(gate, n_qubits, qcirc, first=True):
     pairs = namedtuple("pairs", ["control", "target"])
@@ -95,58 +81,15 @@ def c1c2(gate, n_qubits, qcirc, first=True):
             exponent = exponent - 1
         param = 2 ** exponent
         signal = -1 if (pair.control == 0 and not first) else 1
-        if pair.target == n_qubits - 1:
+        if pair.target == n_qubits - 1 and first:
 
-            csqgate = _xgate(gate, param, signal)
+            csqgate = _gate_u(gate, param, signal)
             qcirc.compose(csqgate, qubits=[pair.control, pair.target], inplace=True)
         else:
             qcirc.crx(signal * np.pi / param, pair.control, pair.target)
 
 
-def c1c2a(gate, coef, n_qubits, qcirc, first=True):
-    """ phase 1 """
-    if not first:
-        for k in range(n_qubits-1):
-            coef[k, n_qubits-2] = - coef[k, n_qubits-2]
-
-    for k in range(n_qubits - 1):
-        line = n_qubits - 2
-        column = k
-        while line >= 0 and column >= 0 and coef[line, column] != 0:
-            control = n_qubits - 2 - column
-            targ = line + 1
-            # -----
-            if first and targ == n_qubits - 1:
-                csqgate = _xgate(gate, coef, column, line)
-                qcirc.compose(csqgate, qubits=[control, targ], inplace=True)
-            else:
-                qcirc.crx(np.pi / coef[line, column], control, targ)
-
-            line = line - 1
-            column = column - 1
-
-
-
-    for k in range(n_qubits - 3, -1, -1):
-        column = n_qubits - 2
-        line = k
-
-
-        while line >= 0 and column >= 0 and coef[line, column] != 0:
-            # -----
-            control = n_qubits - 2 - column
-            targ = line + 1
-            if first and targ == n_qubits - 1:
-                csqgate = _xgate(gate, coef, column, line)
-                qcirc.compose(csqgate, qubits=[control, targ], inplace=True)
-            else:
-                qcirc.crx(np.pi / coef[line, column], control, targ)
-
-            line = line - 1
-            column = column - 1
-
-
-def _xgate(agate, coef, signal):
+def _gate_u(agate, coef, signal):
 
     param = 1/np.abs(coef)
 
@@ -163,40 +106,3 @@ def _xgate(agate, coef, signal):
 
     return csqgate
 
-
-def _coefficients(n_qubits):
-    coef = np.zeros((n_qubits - 1, 2 * n_qubits - 3))
-
-    for i in range(0, n_qubits - 2):
-        one_coef = 2 ** (i+1)
-
-        for j in range(n_qubits - 2, -1, -1):
-            coef[j, i] = one_coef
-            coef[j, (-i + 2 * n_qubits - 4)] = -one_coef
-            one_coef = one_coef // 2
-
-            if one_coef < 2:
-                break
-
-    for k in range(n_qubits - 1):
-        coef[k][n_qubits - 2] = 2 ** k
-
-    return coef
-
-#
-# if __name__ == '__main__':
-#     from qclib.util import get_counts, get_state
-#
-#
-#     qcirc = qiskit.QuantumCircuit(6)
-#     # qcirc.x(0)
-#     qcirc.x(1)
-#     qcirc.x(2)
-#     # qcirc.x(3)
-#     # qcirc.x(4)
-#
-#
-#     toffoli(qcirc, [0, 1, 2, 3, 4], 5)
-#     qcirc.measure_all()
-#     print(get_counts(qcirc))
-#     print(1)
