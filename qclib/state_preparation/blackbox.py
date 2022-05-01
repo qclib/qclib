fc9c242f8cdc4f40b804c1d4640c64aa1eccc6e0
Adenilton Silva <ajsilva@cin.ufpe.br>
2022-05-01 20:21:05 -0300

    black-box state preparation

---
+++ qclib/state_preparation/blackbox.py
@@ -0,0 +1,83 @@
+# Copyright 2021 qclib project.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Black-box state preparation (real amplitudes)
+Grover, Lov K. "Synthesis of quantum superpositions by quantum computation."
+Physical review letters 85.6 (2000): 1334.
+
+Gate U2 in PRL 85.6 (2000) is implemented with uniformly controlled rotations
+"""
+
+import numpy as np
+from qiskit import QuantumCircuit
+from qiskit.extensions import UCRYGate
+from qiskit.extensions import UnitaryGate
+
+def initialize(state_vector):
+    """
+    Blackbox state preparation
+    https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.85.1334
+
+    For instance, to initialize the state a|00> + b|10> (|a|^2+|b|^2=1)
+        $ state = [a, 0, b, 0]
+        $ circuit = initialize(state)
+
+    Parameters
+    ----------
+    state_vector: 2^n dimensional real unit vector
+
+    Returns
+    -------
+    q_circuit: Circuit that initializes a approximated state_vector in a quantum device
+    """
+    n_amplitudes = len(state_vector)
+    n_qubits = float(np.log2(n_amplitudes))
+    if not n_qubits.is_integer():
+        raise Exception("state_vector size is not a power of 2")
+
+    n_qubits = int(n_qubits) + 1
+    theta = np.zeros(n_amplitudes)
+    for k, value in enumerate(state_vector):
+        theta[k] = 2 * np.arccos(value)
+
+    gate_u = QuantumCircuit(n_qubits, name='U')
+    gate_u.h(list(range(1, n_qubits)))
+    ury_gate = UCRYGate(list(theta))
+    gate_u.append(ury_gate, gate_u.qubits)
+    gate_u = gate_u.to_instruction()
+
+    # gate_it = QuantumCircuit(1, name='I_t')
+    it_matrix = [[-1, 0], [0, 1]]
+    gate_it = UnitaryGate(it_matrix)
+
+    gate_is = gate_it.control(n_qubits-1, ctrl_state=0)
+    gate_is.name = 'I_s'
+
+    repetitions = (np.pi/4) * (np.sqrt(n_amplitudes) / np.linalg.norm(state_vector))
+    repetitions = int(repetitions)
+
+    q_circuit = QuantumCircuit(n_qubits)
+    for k in range(repetitions):
+        q_circuit.append(gate_u, q_circuit.qubits)
+        q_circuit.append(gate_it, [0])
+        q_circuit.append(gate_u.inverse(), q_circuit.qubits)
+        q_circuit.append(gate_is, list(range(1, n_qubits)) + [0])
+
+    q_circuit.append(gate_u, q_circuit.qubits)
+
+    if repetitions % 2 == 1:
+        q_circuit.global_phase = np.pi
+
+    return q_circuit
---
+++ test/test_blackbox.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+from qclib.state_preparation.blackbox import initialize
+from qclib.util import get_state
+import numpy as np
+
+
+class TestBdsp(TestCase):
+    def test_blackbox(self):
+        state = np.random.rand(16) - 0.5
+        state = state / np.linalg.norm(state)
+
+        qc = initialize(state)
+        out = get_state(qc)
+
+        out = out.reshape((len(out)//2, 2))
+        out = out[:, 0]
+
+        self.assertTrue(np.allclose(state, out, atol=0.02))
