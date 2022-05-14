991c83dd36c84485041410987c4a66a551de6b40
Adenilton Silva <ajsilva@cin.ufpe.br>
2022-05-14 13:52:42 -0300

    Update test_schmidt.py

--- test/test_schmidt.py
+++ test/test_schmidt.py
@@ -19,13 +19,14 @@ Tests for the schmidt.py module.
 from unittest import TestCase
 import numpy as np
 from itertools import combinations
-from qiskit import ClassicalRegister, execute, transpile
+from qiskit import QuantumCircuit, ClassicalRegister, execute, transpile
 from qiskit.providers.aer.backends import AerSimulator
-from qclib.state_preparation.schmidt import initialize
+from qclib.state_preparation.schmidt import LRInitialize
 from qclib.util import get_state
 
 # pylint: disable=missing-function-docstring
 # pylint: disable=missing-class-docstring
+
 
 class TestSchmidt(TestCase):
     @staticmethod
@@ -34,13 +35,6 @@ class TestSchmidt(TestCase):
         Mean Absolute Error
         """
         return np.sum(np.abs(state-ideal))/len(ideal)
-
-    @staticmethod
-    def fidelity(state1, state2):
-        bra = np.conj(state1)
-        ket = state2
-
-        return np.abs(bra.dot(ket))**2
 
     @staticmethod
     def get_counts(circuit):
@@ -61,7 +55,7 @@ class TestSchmidt(TestCase):
                 counts_with_zeros[pattern] = 0.0
 
         sum_values = sum(counts.values())
-        return [ value/sum_values for (key, value) in counts_with_zeros.items() ]
+        return [value/sum_values for (key, value) in counts_with_zeros.items()]
 
     def _test_initialize_mae(self, rank=0, max_mae=10**-15):
         state_vector = np.random.rand(32) + np.random.rand(32) * 1j
@@ -70,7 +64,9 @@ class TestSchmidt(TestCase):
         qubits = list(range(5))
         partitions = combinations(qubits, 3)
         for partition in partitions:
-            circuit = initialize(state_vector, low_rank=rank, partition=partition)
+            circuit = QuantumCircuit(5)
+            lr_params = {'lr': rank, 'partition': partition}
+            LRInitialize.initialize(circuit, state_vector, lr_params=lr_params)
 
             state = get_state(circuit)
 
@@ -79,45 +75,23 @@ class TestSchmidt(TestCase):
     def test_initialize_full_rank(self):
         state_vector = np.random.rand(32) + np.random.rand(32) * 1j
         state_vector = state_vector / np.linalg.norm(state_vector)
-
-        circuit = initialize(state_vector)
-
-        state = get_state(circuit)
-
-        self.assertTrue(np.allclose(state_vector, state))
-
-    def test_initialize_ame(self):
-        """ Test initialization of a absolutely maximally entangled state"""
-        state_vector = [1, 1, 1, 1,1,-1,-1, 1, 1,-1,-1, 1, 1, 1,1,1,
-                        1, 1,-1,-1,1,-1, 1,-1,-1, 1,-1, 1,-1,-1,1,1]
-        state_vector = state_vector / np.linalg.norm(state_vector)
-
-        circuit = initialize(state_vector)
+        circuit = QuantumCircuit(5)
+        LRInitialize.initialize(circuit, state_vector)
 
         state = get_state(circuit)
 
         self.assertTrue(np.allclose(state_vector, state))
-
-    def test_measurement_full_rank(self):
-        state_vector = np.random.rand(32) + np.random.rand(32) * 1j
-        state_vector = state_vector / np.linalg.norm(state_vector)
-
-        circuit = initialize(state_vector)
-
-        state = TestSchmidt.get_counts(circuit)
-
-        self.assertTrue(np.allclose( np.power(np.abs(state_vector),2), state,
-                        rtol=1e-01, atol=0.005))
 
     def test_initialize_rank_5(self):
         state_vector = np.random.rand(32) + np.random.rand(32) * 1j
         state_vector = state_vector / np.linalg.norm(state_vector)
 
-        circuit = initialize(state_vector, low_rank=5)
+        circuit = QuantumCircuit(5)
+        LRInitialize.initialize(circuit, state_vector, lr_params={'lr': 5})
 
         state = get_state(circuit)
 
-        self.assertTrue(np.allclose(state_vector, state)) # same as unitary.
+        self.assertTrue(np.allclose(state_vector, state))
 
     def test_initialize_rank_4(self):
         self._test_initialize_mae(4, max_mae=10**-14)
@@ -132,6 +106,7 @@ class TestSchmidt(TestCase):
         self._test_initialize_mae(1, max_mae=0.09)
 
     def test_cnot_count_rank_1(self):
+
         # Builds a rank 1 state.
         state_vector = [1]
         for _ in range(5):
@@ -139,8 +114,8 @@ class TestSchmidt(TestCase):
             vec = vec / np.linalg.norm(vec)
             state_vector = np.kron(state_vector, vec)
 
-        circuit = initialize(state_vector)
+        circuit = QuantumCircuit(5)
+        LRInitialize.initialize(circuit, state_vector)
         transpiled_circuit = transpile(circuit, basis_gates=['u', 'cx'], optimization_level=3)
 
-        # The cnots count must be zero (i.e. not present in the dictionary).
         self.assertTrue('cx' not in transpiled_circuit.count_ops())
