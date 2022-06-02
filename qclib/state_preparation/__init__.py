38d6d24eddad0163c603409e79a62f31046869c5
israelferrazaraujo <israelferrazaraujo@hotmail.com>
2022-06-02 10:44:18 -0300

    init file

--- qclib/state_preparation/__init__.py
+++ qclib/state_preparation/__init__.py
@@ -1,2 +1,6 @@
+from .topdown import TopDownInitialize
+from .dcsp import DcspInitialize
+from .bdsp import BdspInitialize
 from .lowrank import LowRankInitialize
+from .baa_lowrank import BaaLowRankInitialize
 from .merge import MergeInitialize
