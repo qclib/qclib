d7609c5fa2511d38c7a0b40f0e8c40e8137073f4
israelferrazaraujo <israelferrazaraujo@hotmail.com>
2022-06-29 23:04:26 -0300

    qclib version upgrade

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="qclib",
-    version="0.0.16",
+    version="0.0.17",
     author="Adenilton Silva",
     author_email="ajsilva@cin.ufpe.br",
     description="A quantum computing library using qiskit",
