74d4ae76645ee8977a25f4e543e405cdd0e7f39f
IsmaelCesar <leamscesar@gmail.com>
2022-08-18 06:21:21 -0300

    Bergholm et al (2005)  State preparation algorithm (#102)
    * Implementing Bergholm et al (2005) state preparation
    
    - Implementing the inverse of the diagonal and applying classicaly to the operators
    - Reusing the UCGate implementation available on qclib
    
    on branch bergholm_sp
    Changes to be submitted:
    	modified:   qclib/state_preparation/__init__.py
    	new file:   qclib/state_preparation/bergholm.py
    	new file:   test/test_bergholm.py
    
    * Bergholm initilizing real value data
    
    on branch bergholm_sp
    changes to be submitted:
    	modified:   qclib/state_preparation/bergholm.py
    	modified:   test/test_bergholm.py
    
    * Update bergholm.py
    
    * rename class
    
    * Update ucg_initialize.py
    
    Co-authored-by: Adenilton Silva <adjs@email.com>
    Co-authored-by: Adenilton Silva <ajsilva@cin.ufpe.br>

---
+++ test/test_bergholm.py
@@ -0,0 +1,55 @@
+# Copyright 2021 qclib project.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+    Tests for the bergholm state preparation
+"""
+from unittest import TestCase
+import numpy as np
+from qiskit import QuantumCircuit
+
+from qclib.state_preparation import UCGInitialize
+from qclib.util import get_state
+
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-class-docstring
+
+class TestUCGInitialize(TestCase):
+
+  def test_three_qubit_state_real(self):
+    state = np.random.rand(8)
+    state = state / np.linalg.norm(state)
+    
+    initialize = UCGInitialize.initialize
+    circuit = QuantumCircuit(3)
+    
+    initialize(circuit, state.tolist())
+
+    output_state = get_state(circuit)
+ 
+    self.assertTrue(np.allclose(output_state, state))
+
+  def test_three_qubit_state_complex(self):
+
+    state = np.random.rand(8) + np.random.rand(8) * 1j
+    state = state / np.linalg.norm(state)
+    
+    initialize = UCGInitialize.initialize
+    circuit = QuantumCircuit(3)
+    
+    initialize(circuit, state.tolist())
+
+    output_state = get_state(circuit)
+ 
+    self.assertTrue(np.allclose(output_state, state))
