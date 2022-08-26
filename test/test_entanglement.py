14bc774ccc76ee03e8bb8f8c9a9ed93103f2ccb4
Adenilton Silva <ajsilva@cin.ufpe.br>
2022-08-26 09:16:45 -0300

    Update test_entanglement.py

--- test/test_entanglement.py
+++ test/test_entanglement.py
@@ -12,24 +12,53 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+""" Tests for qclib.entanglement.py """
+
 from unittest import TestCase
 import numpy as np
 from qclib.entanglement import geometric_entanglement
-from qclib.state_preparation.util.baa import adaptive_approximation
+
 
 class TestEntanglement(TestCase):
+    """Tests geometric entanglement"""
+
     def test_geometric_measure_entanglement(self):
+        """test geometric entanglement with ghz states"""
         ghz4 = np.zeros(16)
         ghz4[0] = 1 / np.sqrt(2)
         ghz4[15] = 1 / np.sqrt(2)
         gme = geometric_entanglement(ghz4)
         self.assertTrue(np.isclose(gme, 0.5))
-        
-        nqbits = 5
-        w6 = np.zeros(2**nqbits)
-        for k in range(nqbits):
-            w6[2**k] = 1 / np.sqrt(nqbits)
 
-        gme = geometric_entanglement(w6)
+        nqbits = 5
+        w6_vector = np.zeros(2**nqbits)
+        for k in range(nqbits):
+            w6_vector[2**k] = 1 / np.sqrt(nqbits)
+
+        gme = geometric_entanglement(w6_vector)
         self.assertTrue(np.abs(gme - 0.59) < 1e-3)
 
+    def test_geometric_positive(self):
+        """Test if geometric entanglemen is positive"""
+        vector = np.array(
+            [
+                0.00186779 + 0.01306012j,
+                -0.01104266 - 0.07721341j,
+                -0.01704493 + 0.00199258j,
+                0.10077226 - 0.01178041j,
+                -0.00188656 - 0.01597529j,
+                0.01115364 + 0.09444834j,
+                0.01555059 + 0.00254013j,
+                -0.09193749 - 0.01501764j,
+                -0.00979644 - 0.06849949j,
+                0.05791804 + 0.40497947j,
+                0.0893996 - 0.01045093j,
+                -0.52854411 + 0.06178752j,
+                0.00989489 + 0.08378937j,
+                -0.05850013 - 0.49537556j,
+                -0.08156188 - 0.01332283j,
+                0.48220629 + 0.07876658j,
+            ]
+        )
+
+        self.assertTrue(geometric_entanglement(vector) > 0)
