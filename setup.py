e1bac4a5f6c073de78863066fd2569f64bf3e4e3
israelferrazaraujo <israelferrazaraujo@hotmail.com>
2022-09-02 22:35:49 -0300

    Upgrade version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="qclib",
-    version="0.0.18",
+    version="0.0.19",
     author="qclib team",
     author_email="ajsilva@cin.ufpe.br",
     description="A quantum computing library using qiskit",
