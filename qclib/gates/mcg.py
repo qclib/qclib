a916f812ed60238550fc4b2c2ee90a890d2be158
israelferrazaraujo <israelferrazaraujo@hotmail.com>
2022-12-26 08:54:11 +0900

    General SU(2) and U(2) gates

--- qclib/gates/mcg.py
+++ qclib/gates/mcg.py
@@ -14,17 +14,17 @@
 
 from typing import Union, List, Tuple
 
-from cmath import isclose
+from cmath import isclose, phase
 import numpy as np
 
 from qiskit import QuantumCircuit, QuantumRegister
 from qiskit.circuit import Qubit
-from qiskit.circuit.library import RZGate, RYGate
-from qiskit.extensions import UnitaryGate
-from qiskit.quantum_info import OneQubitEulerDecomposer
+#from qiskit.circuit.library import RZGate, RYGate
+#from qiskit.extensions import UnitaryGate
+#from qiskit.quantum_info import OneQubitEulerDecomposer
 
-from .mcx_gate import linear_mcx, mcx_v_chain_dirty, LinearMcx
-
+from .mcx_gate import McxVchainDirty
+#from .mcx_gate import linear_mcx, mcx_v_chain_dirty, LinearMcx, McxVchainDirty
 
 
 # pylint: disable=maybe-no-member
@@ -49,10 +49,24 @@ def mcg(
     else:
         if num_ctrl > 1 and _check_su2(unitary):
             linear_mcg_su2(self, unitary, controls, target, ctrl_state)
-        else:
+        elif num_ctrl == 1:
             u_gate = QuantumCircuit(1)
             u_gate.unitary(unitary, 0)
             self.append(u_gate.control(num_ctrl, ctrl_state=ctrl_state), [*controls, target])
+        else:
+            su_2, phase = _u2_to_su2(unitary)
+            self.mcg(su_2, controls, target, ctrl_state)
+
+            diag = np.diag([np.exp(1.0j*phase), np.exp(1.0j*phase)])
+            gate_d = QuantumCircuit(1)
+            gate_d.unitary(diag, 0)
+            self.append(gate_d.control(len(controls), ctrl_state=ctrl_state), [*controls, target])
+
+
+def _u2_to_su2(u_2):
+    phase_factor = np.conj(np.linalg.det(u_2) ** (-1 / u_2.shape[0]))
+    su_2 = u_2 / phase_factor
+    return su_2, phase(phase_factor)
 
 
 def _check_u2(matrix):
@@ -80,24 +94,41 @@ def linear_mcg_su2(
     Multicontrolled gate decomposition with linear cost.
     `unitary` must be a SU(2) matrix with at least one real diagonal.
     """
-    is_main_diag_real = unitary[0, 0].imag == 0.0 and unitary[1, 1].imag == 0.0
-    is_secondary_diag_real = unitary[0,1].imag == 0.0 and unitary[1,0].imag == 0.0
+    is_main_diag_real = isclose(unitary[0, 0].imag, 0.0) and isclose(unitary[1, 1].imag, 0.0)
+    is_secondary_diag_real = isclose(unitary[0,1].imag, 0.0) and isclose(unitary[1,0].imag, 0.0)
 
     if  not is_main_diag_real and not is_secondary_diag_real:
-        linear_depth_any_mcsu2(
-            self,
-            unitary=unitary,
-            controls=controls,
-            target=target,
-            ctrl_state=ctrl_state
-        )
+        # linear_depth_any_mcsu2(
+        #     self,
+        #     unitary=unitary,
+        #     controls=controls,
+        #     target=target,
+        #     ctrl_state=ctrl_state
+        #     )
+        
+        # U = V D V^-1, where the entries of the diagonal D are the eigenvalues
+        # `eig_vals` of U and the column vectors of V are the eigenvectors
+        # `eig_vecs` of U. These columns are orthonormal and the main diagonal
+        # of V is real-valued.
+        eig_vals, eig_vecs = np.linalg.eig(unitary)
+
+        x_vecs, z_vecs = _get_x_z(eig_vecs)
+        x_vals, z_vals = _get_x_z(np.diag(eig_vals))
+
+        action_only = True
+
+        self.h(target)
+        half_linear_depth_mcv(self, x_vecs, z_vecs, controls, target, ctrl_state, action_only=action_only, inverse=True)
+        self.h(target)
+        linear_depth_mcv(self, x_vals, z_vals, controls, target, ctrl_state, action_only=action_only)
+        self.h(target)
+        half_linear_depth_mcv(self, x_vecs, z_vecs, controls, target, ctrl_state, action_only=action_only)
+        self.h(target)
+
     else:
-        if is_secondary_diag_real:
-            x = unitary[0,1]
-            z = unitary[1,1]
-        else:
-            x = -unitary[0,1].real
-            z = unitary[1,1] - unitary[0,1].imag * 1.0j
+        x, z = _get_x_z(unitary)
+
+        if not is_secondary_diag_real:
             self.h(target)
 
         linear_depth_mcv(
@@ -113,13 +144,27 @@ def linear_mcg_su2(
             self.h(target)
 
 
+def _get_x_z(su2):
+    is_secondary_diag_real = isclose(su2[0,1].imag, 0.0) and isclose(su2[1,0].imag, 0.0)
+
+    if is_secondary_diag_real:
+        x = su2[0,1]
+        z = su2[1,1]
+    else:
+        x = -su2[0,1].real
+        z = su2[1,1] - su2[0,1].imag * 1.0j
+
+    return x, z
+
+
 def linear_depth_mcv(
     self,
     x,
     z,
     controls: Union[QuantumRegister, List[Qubit]],
     target: Qubit,
-    ctrl_state: str=None
+    ctrl_state: str=None,
+    action_only=False
 ):
 
     alpha_r = np.sqrt(
@@ -133,6 +178,7 @@ def linear_depth_mcv(
             (z.real + 1.) * (np.sqrt((z.real + 1.) / 2.) + 1.)
         )
     )
+
     s_op = np.array(
         [[alpha, -np.conj(beta)],
         [beta, np.conj(alpha)]]
@@ -144,7 +190,7 @@ def linear_depth_mcv(
 
     num_ctrl = len(controls)
     k_1 = int(np.ceil(num_ctrl / 2.))
-    k_2 = int(np.floor(num_ctrl / 2.)) - 1
+    k_2 = int(np.floor(num_ctrl / 2.))
 
     ctrl_state_k_1 = None
     ctrl_state_k_2 = None
@@ -153,138 +199,179 @@ def linear_depth_mcv(
         ctrl_state_k_1 = ctrl_state[::-1][:k_1][::-1]
         ctrl_state_k_2 = ctrl_state[::-1][k_1:][::-1]
 
-    mcx_v_chain_dirty(
-        self,
-        controls[:k_1],
-        target,
-        controls[k_1:2 * k_1 - 2],
-        ctrl_state_k_1
-    )
+    first_mcx = McxVchainDirty(k_1, ctrl_state=ctrl_state_k_1, action_only=action_only).definition
+    second_mcx = McxVchainDirty(k_2, ctrl_state=ctrl_state_k_2).definition
+
+    self.append(first_mcx.reverse_ops(), controls[:k_1] + controls[k_1:2*k_1 - 2] + [target])
     self.append(s_gate, [target])
-    mcx_v_chain_dirty(
-        self,
-        controls[k_1:],
-        target,
-        controls[k_1 - k_2 + 1:k_1],
-        ctrl_state_k_2
-    )
+
+    self.append(second_mcx, controls[k_1:] + controls[k_1 - k_2 + 2:k_1] + [target])
     self.append(s_gate.inverse(), [target])
-    mcx_v_chain_dirty(
-        self,
-        controls[:k_1],
-        target,
-        controls[k_1:2 * k_1 - 2],
-        ctrl_state_k_1
-    )
+
+    first_mcx = McxVchainDirty(k_1, ctrl_state=ctrl_state_k_1, action_only=False).definition
+    self.append(first_mcx, controls[:k_1] + controls[k_1:2*k_1 - 2] + [target])
     self.append(s_gate, [target])
-    mcx_v_chain_dirty(
-        self,
-        controls[k_1:],
-        target,
-        controls[k_1 - k_2 + 1:k_1],
-        ctrl_state_k_2
-    )
+
+    self.append(second_mcx, controls[k_1:] + controls[k_1 - k_2 + 2:k_1] + [target])
     self.append(s_gate.inverse(), [target])
 
 
-def linear_depth_any_mcsu2(
+def half_linear_depth_mcv(
     self,
-    unitary,
+    x,
+    z,
     controls: Union[QuantumRegister, List[Qubit]],
     target: Qubit,
-    ctrl_state: str=None
+    ctrl_state: str=None,
+    action_only: bool=False,
+    inverse: bool=False
 ):
-    """
-        Implements the gate decompostion of any gate in SU(2)
-        presented in Lemma 7.9 in Barenco et al. 1995 (arXiv:quant-ph/9503016)
-    """
-    theta, phi, lamb, _ = OneQubitEulerDecomposer._params_zyz(unitary)
 
-    a_gate, b_gate, c_gate = get_abc_operators(phi, theta, lamb)
+    alpha_r = np.sqrt((z.real + 1.) / 2.)
+    alpha_i = z.imag / np.sqrt(2*(z.real + 1.))
+    alpha = alpha_r + 1.j * alpha_i
 
-    _apply_abc(
-        self,
-        controls=controls,
-        target=target,
-        su2_gates=(a_gate, b_gate, c_gate),
-        ctrl_state=ctrl_state
+    beta = x / np.sqrt(2*(z.real + 1.))
+    
+    s_op = np.array(
+        [[alpha, -np.conj(beta)],
+        [beta, np.conj(alpha)]]
     )
 
+    # S gate definition
+    s_gate = QuantumCircuit(1)
+    s_gate.unitary(s_op, 0)
 
-def get_abc_operators(beta, gamma, delta):
-    """
-    Creates A,B and C matrices such that
-    ABC = I
-    """
-    # A
-    a_rz = RZGate(beta).to_matrix()
-    a_ry = RYGate(gamma / 2).to_matrix()
-    a_matrix = a_rz.dot(a_ry)
+    num_ctrl = len(controls)
+    k_1 = int(np.ceil(num_ctrl / 2.))
+    k_2 = int(np.floor(num_ctrl / 2.))
 
-    # B
-    b_ry = RYGate(-gamma / 2).to_matrix()
-    b_rz = RZGate(-(delta + beta) / 2).to_matrix()
-    b_matrix = b_ry.dot(b_rz)
-
-    # C
-    c_matrix = RZGate((delta - beta) / 2).to_matrix()
-
-    a_gate = UnitaryGate(a_matrix, label='A')
-    b_gate = UnitaryGate(b_matrix, label='B')
-    c_gate = UnitaryGate(c_matrix, label='C')
-
-    return a_gate, b_gate, c_gate
-
-
-def _apply_abc(
-    self,
-    controls: Union[QuantumRegister, List[Qubit]],
-    target: Qubit,
-    su2_gates: Tuple[UnitaryGate],
-    ctrl_state: str = None
-):
-    """
-        Applies ABC matrices to the quantum circuit according to theorem 5
-        of Iten et al. 2016 (arXiv:1501.06911).
-        Where su2_gates is a tuple of UnitaryGates in SU(2) eg.: (A,  B, C).
-    """
-    a_gate, b_gate, c_gate = su2_gates
+    ctrl_state_k_1 = None
+    ctrl_state_k_2 = None
 
     if ctrl_state is not None:
-        for i, ctrl in enumerate(ctrl_state[::-1]):
-            if ctrl == '0':
-                self.x(controls[i])
+        ctrl_state_k_1 = ctrl_state[::-1][:k_1][::-1]
+        ctrl_state_k_2 = ctrl_state[::-1][k_1:][::-1]
 
-    if len(controls) < 3:
+    second_mcx = McxVchainDirty(k_2, ctrl_state=ctrl_state_k_2).definition
 
-        self.append(c_gate, [target])
-        self.mcx(controls, [target])
-        self.append(b_gate, [target])
-        self.mcx(controls, [target])
-        self.append(a_gate, [target])
+    if inverse:
+        first_mcx = McxVchainDirty(k_1, ctrl_state=ctrl_state_k_1, action_only=action_only).definition
+
+        self.append(s_gate, [target])
+        self.append(second_mcx, controls[k_1:] + controls[k_1 - k_2 + 2:k_1] + [target])
+        
+        self.append(s_gate.inverse(), [target])
+        self.append(first_mcx, controls[:k_1] + controls[k_1:2*k_1 - 2] + [target])
 
     else:
-        ancilla = controls[-1]
+        first_mcx = McxVchainDirty(k_1, ctrl_state=ctrl_state_k_1, action_only=False).definition
 
-        mcx_gate = LinearMcx(len(controls[:-1]), action_only=True).definition
+        self.append(first_mcx, controls[:k_1] + controls[k_1:2*k_1 - 2] + [target])
+        self.append(s_gate, [target])
 
-        # setting gate controls
-        controlled_a = a_gate.control(1)
-        controlled_b = b_gate.control(1)
-        controlled_c = c_gate.control(1)
+        self.append(second_mcx, controls[k_1:] + controls[k_1 - k_2 + 2:k_1] + [target])
+        self.append(s_gate.inverse(), [target])
 
-        # applying controlled_gates to circuit
-        self.append(controlled_c, [ancilla, target])
 
-        # linear_mcx(self, controls[:-1], [target], [ancilla])
-        self.append(mcx_gate, controls[:-1] +[target] + [ancilla])
-        self.append(controlled_b, [ancilla, target])
-        # linear_mcx(self, controls[:-1], [target], [ancilla])
-        self.append(mcx_gate.inverse(), controls[:-1] + [target] + [ancilla])
+# def linear_depth_any_mcsu2(
+#     self,
+#     unitary,
+#     controls: Union[QuantumRegister, List[Qubit]],
+#     target: Qubit,
+#     ctrl_state: str=None
+# ):
+#     """
+#         Implements the gate decompostion of any gate in SU(2)
+#         presented in Lemma 7.9 in Barenco et al. 1995 (arXiv:quant-ph/9503016)
+#     """
+#     theta, phi, lamb, _ = OneQubitEulerDecomposer._params_zyz(unitary)
 
-        self.append(controlled_a, [ancilla, target])
+#     a_gate, b_gate, c_gate = get_abc_operators(phi, theta, lamb)
 
-    if ctrl_state is not None:
-        for i, ctrl in enumerate(ctrl_state[::-1]):
-            if ctrl == '0':
-                self.x(controls[i])
+#     _apply_abc(
+#         self,
+#         controls=controls,
+#         target=target,
+#         su2_gates=(a_gate, b_gate, c_gate),
+#         ctrl_state=ctrl_state
+#     )
+
+
+# def get_abc_operators(beta, gamma, delta):
+#     """
+#     Creates A,B and C matrices such that
+#     ABC = I
+#     """
+#     # A
+#     a_rz = RZGate(beta).to_matrix()
+#     a_ry = RYGate(gamma / 2).to_matrix()
+#     a_matrix = a_rz.dot(a_ry)
+
+#     # B
+#     b_ry = RYGate(-gamma / 2).to_matrix()
+#     b_rz = RZGate(-(delta + beta) / 2).to_matrix()
+#     b_matrix = b_ry.dot(b_rz)
+
+#     # C
+#     c_matrix = RZGate((delta - beta) / 2).to_matrix()
+
+#     a_gate = UnitaryGate(a_matrix, label='A')
+#     b_gate = UnitaryGate(b_matrix, label='B')
+#     c_gate = UnitaryGate(c_matrix, label='C')
+
+#     return a_gate, b_gate, c_gate
+
+
+# def _apply_abc(
+#     self,
+#     controls: Union[QuantumRegister, List[Qubit]],
+#     target: Qubit,
+#     su2_gates: Tuple[UnitaryGate],
+#     ctrl_state: str = None
+# ):
+#     """
+#         Applies ABC matrices to the quantum circuit according to theorem 5
+#         of Iten et al. 2016 (arXiv:1501.06911).
+#         Where su2_gates is a tuple of UnitaryGates in SU(2) eg.: (A,  B, C).
+#     """
+#     a_gate, b_gate, c_gate = su2_gates
+
+#     if ctrl_state is not None:
+#         for i, ctrl in enumerate(ctrl_state[::-1]):
+#             if ctrl == '0':
+#                 self.x(controls[i])
+
+#     if len(controls) < 3:
+
+#         self.append(c_gate, [target])
+#         self.mcx(controls, [target])
+#         self.append(b_gate, [target])
+#         self.mcx(controls, [target])
+#         self.append(a_gate, [target])
+
+#     else:
+#         ancilla = controls[-1]
+
+#         mcx_gate = LinearMcx(len(controls[:-1]), action_only=True).definition
+
+#         # setting gate controls
+#         controlled_a = a_gate.control(1)
+#         controlled_b = b_gate.control(1)
+#         controlled_c = c_gate.control(1)
+
+#         # applying controlled_gates to circuit
+#         self.append(controlled_c, [ancilla, target])
+
+#         # linear_mcx(self, controls[:-1], [target], [ancilla])
+#         self.append(mcx_gate, controls[:-1] + [target] + [ancilla])
+#         self.append(controlled_b, [ancilla, target])
+#         # linear_mcx(self, controls[:-1], [target], [ancilla])
+#         self.append(mcx_gate.inverse(), controls[:-1] + [target] + [ancilla])
+
+#         self.append(controlled_a, [ancilla, target])
+
+#     if ctrl_state is not None:
+#         for i, ctrl in enumerate(ctrl_state[::-1]):
+#             if ctrl == '0':
+#                 self.x(controls[i])
