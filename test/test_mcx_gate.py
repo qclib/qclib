1b6a4640b902cc26445dbe736d5925f22936beeb
Ismael Cesar <leamscesar@gmail.com>
2022-12-26 16:57:49 -0300

    Adding comparison loop for methods LienarMcx and McxVcainDirty

--- test/test_mcx_gate.py
+++ test/test_mcx_gate.py
@@ -17,44 +17,96 @@
 from unittest import TestCase
 
 import numpy as np
+from typing import Union
 from qiskit import QuantumCircuit, QuantumRegister, transpile
 from qiskit.quantum_info import Operator
 from qclib.gates.mcx_gate import mcx_v_chain_dirty
 from qclib.gates.mcx_gate import linear_mcx, McxVchainDirty, LinearMcx
 
 
+def _operator_cmp_loop(
+    self,
+    qubit_range,
+    McxMethod: Union[LinearMcx, McxVchainDirty],
+    action_only=False
+):
+    """
+    Compares if the custom operator defined by the custom MCX method is the same
+    as the one defined by Qiskit MCX method.
+    Parameters
+    ----------
+    params: 
+        - qubit_range: The number of qubits with which our method needs to be tested
+        - McxMethod: The class definition of the method to be used. It must be either
+                     `LinearMcx` or `McxVchainDirty`
+        - action_only: Decide wether or not use only the action of the V-Chain of Toffoli
+                       gates
+    """
+    for num_qubits in qubit_range:
+        mcx_method = McxMethod(num_qubits-2, action_only=action_only).definition
+
+        mcx_qiskit = QuantumCircuit(num_qubits)
+
+        mcx_qiskit.mcx(
+            control_qubits=list(range(num_qubits - 2)),
+            target_qubit=num_qubits - 2,
+            ancilla_qubits=num_qubits - 1,
+            mode="recursion"
+        )
+
+        mcx_method_op = Operator(mcx_method).data
+        mcx_qiskit_op = Operator(mcx_qiskit).data
+
+        self.assertTrue(np.allclose(mcx_method_op, mcx_qiskit_op))
+
 class TestLinearMCX(TestCase):
     """ Testing qclib.gates.mcx_gate """
 
-    QuantumCircuit.mcx_v_chain_dirty = mcx_v_chain_dirty
+    #QuantumCircuit.mcx_v_chain_dirty = mcx_v_chain_dirty
     QuantumCircuit.linear_mcx = linear_mcx
 
+    def test_linear_mcx(self):
+        """ Test if linear_mcx is correct """
+        _operator_cmp_loop(
+            self,
+            range(8, 9),
+            LinearMcx
+        )
 
-    def test_mcx_v_chain_dirty(self):
-        """ Test if mcx_v_chain_dirty is correct """
+    def test_linear_mcx_action_only(self):
+        """ Test if linear_mcx is correct """
+        _operator_cmp_loop(
+            self,
+            range(8, 9),
+            LinearMcx,
+            action_only=True
+        )
 
-        for num_controls in range(6, 8):
-            num_ancilla = num_controls - 2
-            control_qubits = QuantumRegister(num_controls)
-            ancilla_qubits = QuantumRegister(num_ancilla)
-            target_qubit = QuantumRegister(1)
+    def test_linear_mcx_depth(self):
+        """ Test linear_mcx depth"""
 
-            mcx_v_chain = McxVchainDirty(num_controls).definition
+        for num_qubits in range(30, 31):
 
-            mcx_v_chain_qiskit = QuantumCircuit(control_qubits, ancilla_qubits, target_qubit)
+            mcx_dirty_ancilla = LinearMcx(num_qubits-2).definition
 
-            mcx_v_chain_qiskit.mcx(
-                control_qubits=control_qubits,
-                target_qubit=target_qubit,
-                ancilla_qubits=ancilla_qubits,
-                mode="v-chain-dirty"
+            mcx_qiskit = QuantumCircuit(num_qubits)
+
+            mcx_qiskit.mcx(
+                control_qubits=list(range(num_qubits - 2)),
+                target_qubit=num_qubits - 2,
+                ancilla_qubits=num_qubits - 1,
+                mode="recursion"
             )
 
-            mcx_v_chain_op = Operator(mcx_v_chain).data
-            mcx_v_chain_qiskit_op = Operator(mcx_v_chain_qiskit).data
+            tr_mcx_dirty_ancilla = transpile(mcx_dirty_ancilla, basis_gates=['u', 'cx'])
+            tr_mcx_qiskit = transpile(mcx_qiskit, basis_gates=['u', 'cx'])
 
-            self.assertTrue(np.allclose(mcx_v_chain_op, mcx_v_chain_qiskit_op))
+            self.assertLess(tr_mcx_dirty_ancilla.depth(), tr_mcx_qiskit.depth())
 
+
+class TestMcxVchainDirty(TestCase):
+
+    QuantumCircuit.mcx_v_chain_dirty = mcx_v_chain_dirty
 
     def test_mcx_v_chain_dirty_depth(self):
         """ Test mcx_v_chain_dirty depth"""
@@ -82,46 +134,19 @@ class TestLinearMCX(TestCase):
             self.assertTrue(8 * num_controls - 6 == tr_mcx_v_chain.count_ops()['cx'])
             self.assertLess(tr_mcx_v_chain.depth(), tr_mcx_v_chain_qiskit.depth())
 
+    def test_mcx_v_chain_dirty(self):
+        """ Test if mcx_v_chain_dirty is correct """
+        _operator_cmp_loop(
+            self,
+            range(6, 8),
+            McxVchainDirty
+        )
 
-    def test_linear_mcx(self):
-        """ Test if linear_mcx is correct """
-
-        for num_qubits in range(8, 9):
-
-            mcx_dirty_ancilla = LinearMcx(num_qubits-2).definition
-
-            mcx_qiskit = QuantumCircuit(num_qubits)
-
-            mcx_qiskit.mcx(
-                control_qubits=list(range(num_qubits - 2)),
-                target_qubit=num_qubits - 2,
-                ancilla_qubits=num_qubits - 1,
-                mode="recursion"
-            )
-
-            mcx_dirty_ancilla_op = Operator(mcx_dirty_ancilla).data
-            mcx_qiskit_op = Operator(mcx_qiskit).data
-
-            self.assertTrue(np.allclose(mcx_dirty_ancilla_op, mcx_qiskit_op))
-
-
-    def test_linear_mcx_depth(self):
-        """ Test linear_mcx depth"""
-
-        for num_qubits in range(30, 31):
-
-            mcx_dirty_ancilla = LinearMcx(num_qubits-2).definition
-
-            mcx_qiskit = QuantumCircuit(num_qubits)
-
-            mcx_qiskit.mcx(
-                control_qubits=list(range(num_qubits - 2)),
-                target_qubit=num_qubits - 2,
-                ancilla_qubits=num_qubits - 1,
-                mode="recursion"
-            )
-
-            tr_mcx_dirty_ancilla = transpile(mcx_dirty_ancilla, basis_gates=['u', 'cx'])
-            tr_mcx_qiskit = transpile(mcx_qiskit, basis_gates=['u', 'cx'])
-
-            self.assertLess(tr_mcx_dirty_ancilla.depth(), tr_mcx_qiskit.depth())
+    def test_mcx_v_chain_dirty_action_only(self):
+        """ Test if mcx_v_chain_dirty is correct with action only"""
+        _operator_cmp_loop(
+            self,
+            range(6, 8),
+            McxVchainDirty,
+            action_only=True
+        )
