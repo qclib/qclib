224b21302af7f785969dc09995c76d9cedc0268e
Israel F. Araujo <israelferrazaraujo@hotmail.com>
2023-09-22 12:56:29 +0900

    Sparse measurement

--- test/util.py
+++ test/util.py
@@ -1,7 +1,7 @@
 """ auxiliary file """
 from qiskit import execute
 
-def measurement(circuit, n_qubits, classical_register, backend, shots):
+def measurement(circuit, n_qubits, classical_register, backend, shots, dense=True, patterns=None):
     """ run circuit and return measurements """
     circuit.measure(list(range(n_qubits)), classical_register)
 
@@ -9,12 +9,20 @@ def measurement(circuit, n_qubits, classical_register, backend, shots):
 
     counts = job.result().get_counts(circuit)
 
-    counts2 = {}
-    for k in range(2 ** n_qubits):
-        pattern = f'{k:0{n_qubits}b}'
-        if pattern in counts:
-            counts2[pattern] = counts[pattern]
-        else:
-            counts2[pattern] = 0.0
+    if dense:
+        counts2 = {}
+        for k in range(2 ** n_qubits):
+            pattern = f'{k:0{n_qubits}b}'
+            if pattern in counts:
+                counts2[pattern] = counts[pattern]
+            else:
+                counts2[pattern] = 0
+        counts = counts2
+    else:
+        if patterns:
+            for pattern in patterns:
+                if not pattern in counts:
+                    counts[pattern] = 0
+        counts = {i: counts[i] for i in sorted(counts)}
 
-    return [value / shots for (key, value) in counts2.items()]
+    return [value / shots for (_, value) in counts.items()]
