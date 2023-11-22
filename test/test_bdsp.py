1c272cfffcc4e71fe092d0d1b9933dfcd1e6c91a
Jefferson Deyvis <deyvis356@gmail.com>
2023-11-22 17:09:24 -0300

    update files

--- test/test_bdsp.py
+++ test/test_bdsp.py
@@ -19,7 +19,7 @@ import numpy as np
 from qiskit import ClassicalRegister
 from qiskit_aer import AerSimulator
 from qclib.state_preparation import BdspInitialize
-from util import measurement
+from .util import measurement
 
 backend = AerSimulator()
 SHOTS = 8192
