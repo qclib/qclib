43a5554863b72fce3aaeb11ff6da519b4272f5dc
israelferrazaraujo <israelferrazaraujo@hotmail.com>
2024-02-14 06:50:28 +0900

    Sets the minimum version of qiskit to 0.46.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setuptools.setup(
     python_requires='>=3.8',
     install_requires=[
         'scipy>=1.7.1',
-        'qiskit>=0.39.5',
+        'qiskit>=0.46.0',
         'deprecation',
         'tensorly>=0.8.0',
         'keras',
